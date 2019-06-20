52d3b2be6182ea41f0934b630eeb93a60fd65257
matplo <ploskon@gmail.com>
2019-06-20 01:45:00 -0700

    towards an analysis - pythia events / fj::pseudo jet persistency

---
+++ setup_env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function thisdir()
+{
+	SOURCE="${BASH_SOURCE[0]}"
+	while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+	  SOURCE="$(readlink "$SOURCE")"
+	  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	done
+	DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+	echo ${DIR}
+}
+STHISDIR=$(thisdir)
+
+if [ -z ${1} ]; then
+	. ${STHISDIR}/.config_fjpydev
+	echo "[i] FJPYDEV=${FJPYDEV}"
+	. ${FJPYDEV}/test/setup_env.sh
+else
+	echo "[i] using ${1} for fjpydev location"
+fi
+
+export PYTHONPATH=${PYTHONPATH}:${STHISDIR}
+
