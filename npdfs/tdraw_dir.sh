da8e993cf44eb476141b7a45c9ef0deda1f180ac
matplo <ploskon@gmail.com>
2019-07-09 01:34:19 -0700

    tuneups

--- npdfs/tdraw_dir.sh
+++ npdfs/tdraw_dir.sh
@@ -22,7 +22,7 @@ function write_tdraw_file()
 	echo "        nentries =" >> ${outputfile}
 	echo "        firstentry =" >> ${outputfile}
 	echo "        x = 1, 100" >> ${outputfile}
-	echo "        nbinsx = 10" >> ${outputfile}
+	echo "        nbinsx = 20" >> ${outputfile}
 	echo "        logx = True" >> ${outputfile}
 	echo "        x_title = "p_{T}"" >> ${outputfile}
 	echo "        y_title = counts" >> ${outputfile}
@@ -34,28 +34,103 @@ function write_draw_file()
 {
 	inputdir=${1}
 	outputfile=${2}
+	hname=${3}
 	rm ${outputfile}
+	echo "#-----------------------" >> ${outputfile}
+	echo "#figure" >> ${outputfile}
+	echo "#geom 500x500" >> ${outputfile}
+	echo "#ate" >> ${outputfile}
+	echo "#title: smart group" >> ${outputfile}
+	echo "#logy 1" >> ${outputfile}
+	echo "#logx 1" >> ${outputfile}
+	echo "#x p_{T}^{jet} (GeV/c)" >> ${outputfile}
+	echo "#y d#sigma/dp_{T} (arb. units)" >> ${outputfile}
+	echo "#legend pos=ur title=p-Pb w/ EPPS16 Pb^{208} nPDF" >> ${outputfile}
+	echo "#xrange 1, 100" >> ${outputfile}
+	echo "##miny 1e-5" >> ${outputfile}
+	echo "##maxy 1e3" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_uncerts_output_tdraw.root 		:${hname}_set0_to_graph :p  					: title=nPDF #pi^{0}" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_uncerts_output_tdraw.root		:${hname}_epps16_uncerts :p -k serror noleg +a10 : title=uncerts graph" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_output_tdraw.root 				:${hname} 			:p -k +l9 				: title=free proton PDF #pi^{0}" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_charm_uncerts_output_tdraw.root 	:${hname}_set0_to_graph :p  					: title=nPDF D^{0}" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_charm_uncerts_output_tdraw.root	:${hname}_epps16_uncerts :p -k serror noleg +a10 : title=uncerts graph" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_charm_output_tdraw.root 			:${hname} 			:p -k +l9  				: title=free proton PDF D^{0}" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_photon_uncerts_output_tdraw.root :${hname}_set0_to_graph :p  					: title=nPDF #gamma" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_photon_uncerts_output_tdraw.root	:${hname}_epps16_uncerts :p -k serror noleg +a10 : title=uncerts graph" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_photon_output_tdraw.root 		:${hname} 			:p -k +l9 				: title=free proton PDF #gamma" >> ${outputfile}
+	echo
 	echo "#-----------------------" >> ${outputfile}
 	echo "#figure" >> ${outputfile}
 	echo "#geom 500x500" >> ${outputfile}
 	echo "#date" >> ${outputfile}
 	echo "#title: smart group" >> ${outputfile}
 	echo "##logy 1" >> ${outputfile}
-	echo "##logx 1" >> ${outputfile}
+	echo "#logx 1" >> ${outputfile}
 	echo "#x p_{T}^{jet} (GeV/c)" >> ${outputfile}
 	echo "#y ratio  " >> ${outputfile}
 	echo "#legend pos=up title=p-Pb EPPS16 Pb^{208} nPDF/ proton PDF (default PYTHIA8.235),, tx_size=0.04" >> ${outputfile}
 	echo "##comment item=anti-k_{T} R=0.4 jets |#eta_{jet}| < 3 tx_size=0.04 pos=up" >> ${outputfile}
 	echo "#miny 0" >> ${outputfile}
 	echo "#maxy 2" >> ${outputfile}
-	echo "#xrange 1, 20" >> ${outputfile}
-	echo "#line 1, 1, 20, 1, 1, 9, 1" >> ${outputfile}
-	echo "${inputdir}/pPb_npdf_compare_uncerts_ratio_tdraw.root         :epps16_uncerts_ratio_noErr :p l +w3 +k1 : title=#pi^{0} #hat{p_{T}} > 2 " >> ${outputfile}
-	echo "${inputdir}/pPb_npdf_compare_uncerts_ratio_tdraw.root         :epps16_uncerts_ratio :p serror noleg -k: title=Graph" >> ${outputfile}
-	echo "${inputdir}/pPb_npdf_compare_charm_uncerts_ratio_tdraw.root   :epps16_uncerts_ratio_noErr :p l +w3 +k2 : title=D^{0} #hat{p_{T}} > 2 " >> ${outputfile}
-	echo "${inputdir}/pPb_npdf_compare_charm_uncerts_ratio_tdraw.root   :epps16_uncerts_ratio :p serror noleg -k: title=Graph" >> ${outputfile}
-	echo "${inputdir}/pPb_npdf_compare_photon_uncerts_ratio_tdraw.root  :epps16_uncerts_ratio_noErr :p l +w3 +k4 : title=#gamma #hat{p_{T}} > 2 " >> ${outputfile}
-	echo "${inputdir}/pPb_npdf_compare_photon_uncerts_ratio_tdraw.root  :epps16_uncerts_ratio :p serror noleg -k: title=Graph" >> ${outputfile}
+	echo "#xrange 1, 100" >> ${outputfile}
+	echo "##line 1, 1, 20, 1, 1, 9, 1" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_uncerts_ratio_tdraw.root         :${hname}_epps16_uncerts_ratio_noErr :p +w2 +k1 : title=#pi^{0} #hat{p_{T}} > 2 " >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_uncerts_ratio_tdraw.root         :${hname}_epps16_uncerts_ratio :p serror noleg -k: title=Graph" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_charm_uncerts_ratio_tdraw.root   :${hname}_epps16_uncerts_ratio_noErr :p +w2 +k2 : title=D^{0} #hat{p_{T}} > 2 " >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_charm_uncerts_ratio_tdraw.root   :${hname}_epps16_uncerts_ratio :p serror noleg -k: title=Graph" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_photon_uncerts_ratio_tdraw.root  :${hname}_epps16_uncerts_ratio_noErr :p +w2 +k4 : title=#gamma #hat{p_{T}} > 2 " >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_photon_uncerts_ratio_tdraw.root  :${hname}_epps16_uncerts_ratio :p serror noleg -k: title=Graph" >> ${outputfile}
+	echo "#-----------------------" >> ${outputfile}
+	echo "#figure" >> ${outputfile}
+	echo "#geom 500x500" >> ${outputfile}
+	echo "#date" >> ${outputfile}
+	echo "#title: smart group" >> ${outputfile}
+	echo "##logy 1" >> ${outputfile}
+	echo "#logx 1" >> ${outputfile}
+	echo "#x p_{T}^{jet} (GeV/c)" >> ${outputfile}
+	echo "#y ratio  " >> ${outputfile}
+	echo "#legend pos=up title=p-Pb EPPS16 Pb^{208} nPDF/ proton PDF (default PYTHIA8.235),, tx_size=0.04" >> ${outputfile}
+	echo "##comment item=anti-k_{T} R=0.4 jets |#eta_{jet}| < 3 tx_size=0.04 pos=up" >> ${outputfile}
+	echo "#miny 0" >> ${outputfile}
+	echo "#maxy 2" >> ${outputfile}
+	echo "#xrange 1, 100" >> ${outputfile}
+	echo "##line 1, 1, 20, 1, 1, 9, 1" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_uncerts_ratio_tdraw.root         :${hname}_epps16_uncerts_ratio_noErr :p +w2 +k1 : title=#pi^{0} #hat{p_{T}} > 2 " >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_uncerts_ratio_tdraw.root         :${hname}_epps16_uncerts_ratio :p serror noleg -k: title=Graph" >> ${outputfile}
+	echo "#-----------------------" >> ${outputfile}
+	echo "#figure" >> ${outputfile}
+	echo "#geom 500x500" >> ${outputfile}
+	echo "#date" >> ${outputfile}
+	echo "#title: smart group" >> ${outputfile}
+	echo "##logy 1" >> ${outputfile}
+	echo "#logx 1" >> ${outputfile}
+	echo "#x p_{T}^{jet} (GeV/c)" >> ${outputfile}
+	echo "#y ratio  " >> ${outputfile}
+	echo "#legend pos=up title=p-Pb EPPS16 Pb^{208} nPDF/ proton PDF (default PYTHIA8.235),, tx_size=0.04" >> ${outputfile}
+	echo "##comment item=anti-k_{T} R=0.4 jets |#eta_{jet}| < 3 tx_size=0.04 pos=up" >> ${outputfile}
+	echo "#miny 0" >> ${outputfile}
+	echo "#maxy 2" >> ${outputfile}
+	echo "#xrange 1, 100" >> ${outputfile}
+	echo "##line 1, 1, 20, 1, 1, 9, 1" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_charm_uncerts_ratio_tdraw.root   :${hname}_epps16_uncerts_ratio_noErr :p +w2 +k2 : title=D^{0} #hat{p_{T}} > 2 " >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_charm_uncerts_ratio_tdraw.root   :${hname}_epps16_uncerts_ratio :p serror noleg -k: title=Graph" >> ${outputfile}
+	echo "#-----------------------" >> ${outputfile}
+	echo "#figure" >> ${outputfile}
+	echo "#geom 500x500" >> ${outputfile}
+	echo "#date" >> ${outputfile}
+	echo "#title: smart group" >> ${outputfile}
+	echo "##logy 1" >> ${outputfile}
+	echo "#logx 1" >> ${outputfile}
+	echo "#x p_{T}^{jet} (GeV/c)" >> ${outputfile}
+	echo "#y ratio  " >> ${outputfile}
+	echo "#legend pos=up title=p-Pb EPPS16 Pb^{208} nPDF/ proton PDF (default PYTHIA8.235),, tx_size=0.04" >> ${outputfile}
+	echo "##comment item=anti-k_{T} R=0.4 jets |#eta_{jet}| < 3 tx_size=0.04 pos=up" >> ${outputfile}
+	echo "#miny 0" >> ${outputfile}
+	echo "#maxy 2" >> ${outputfile}
+	echo "#xrange 1, 100" >> ${outputfile}
+	echo "##line 1, 1, 20, 1, 1, 9, 1" >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_photon_uncerts_ratio_tdraw.root  :${hname}_epps16_uncerts_ratio_noErr :p +w2 +k4 : title=#gamma #hat{p_{T}} > 2 " >> ${outputfile}
+	echo "${inputdir}/pPb_npdf_compare_photon_uncerts_ratio_tdraw.root  :${hname}_epps16_uncerts_ratio :p serror noleg -k: title=Graph" >> ${outputfile}
 }
 
 indir=${1}
@@ -75,6 +150,6 @@ done
 
 draw_file=$(basename ${indir})
 draw_file="${tdraw_file}.draw"
-write_draw_file ${indir} ${draw_file}
+write_draw_file ${indir} ${draw_file} pt
 
 echo "[i] ${draw_file} written."
