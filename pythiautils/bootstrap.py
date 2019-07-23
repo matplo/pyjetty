572ed3cddab8999cf393bbfc8ad3de70e9c966a4
matplo <ploskon@gmail.com>
2019-07-23 14:46:12 -0700

    run pythia example

--- pythiautils/bootstrap.py
+++ pythiautils/bootstrap.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 
-# copy that file somewhere and start generating...
-
 from pythiautils import configuration as pyconf
 import argparse
 import os
-
+import tqdm
 
 def main(args):
 	print(args)
 	mycfg = []
 	pythia = pyconf.create_and_init_pythia_from_args(args, mycfg)
 
+	for i in tqdm.tqdm(range(args.nev)):
+		if not pythia.next():
+			continue
+	pythia.stat()
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='some pythia simulation', prog=os.path.basename(__file__))
+	parser = argparse.ArgumentParser(description='jet reco on alice data', prog=os.path.basename(__file__))
 	pyconf.add_standard_pythia_args(parser)
 	args = parser.parse_args()	
 	main(args)
