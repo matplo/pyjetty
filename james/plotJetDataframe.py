d091cd8a5860e0b7e67464c9eace09519f95e3de
James Mulligan <james.mulligan@berkeley.edu>
2019-07-23 20:10:14 -0700

    transform track dataframe into series of fastjet particles at initialization

--- james/plotJetDataframe.py
+++ james/plotJetDataframe.py
@@ -43,16 +43,32 @@ def plotJetDataframe(inputFileTrack, outputDir, fileFormat):
     os.makedirs(outputDir)
 
   # Load dataframe from .pkl file
-  # dfTrack is a dataframe with one entry per jet constituent: EventID, JetID, TrackPt, TrackEta, TrackPhi
-  print('Load dataframe and unpickle...')
+  # dfTrack is a dataframe with one row per jet constituent: run_number, ev_id, ParticlePt, ParticleEta, ParticlePhi
+  print('--- %s seconds ---' % (time.time() - start_time))
+  print('Load track dataframe and unpickle...')
   dfTrack = pd.read_pickle(inputFileTrack)
   if debugLevel > 0:
     print(dfTrack.dtypes)
   if debugLevel > 1:
     print(dfTrack)
 
-  # Print number of events
+  # Transform the track dataframe into a SeriesGroupBy object of fastjet particles per event
+  # This is currently the slowest part of the analysis, by factor ~10...
+  print('--- %s seconds ---' % (time.time() - start_time))
+  print('Transform the track dataframe into a series object of fastjet particles per event...')
+
+  # (i) Group the track dataframe by event
+  #     df_tracks is a DataFrameGroupBy object with one track dataframe per event
   df_tracks = dfTrack.groupby(['run_number','ev_id'])
+
+  # (ii) Transform the DataFrameGroupBy object to a SeriesGroupBy of fastjet particles
+  df_fjparticles = df_tracks.apply(get_fjparticles)
+  if debugLevel > 0:
+    print(df_fjparticles.dtypes)
+    print(df_fjparticles)
+  print('--- %s seconds ---' % (time.time() - start_time))
+
+  # Print number of events
   nEvents = df_tracks.size().count()
   print('Number of events: {}'.format(nEvents))
   nTracks = len(dfTrack.index)
@@ -63,13 +79,29 @@ def plotJetDataframe(inputFileTrack, outputDir, fileFormat):
   
   # Find jets and fill histograms
   print('Find jets...')
-  findJets(df_tracks, hDict, outputDir, fileFormat)
+  analyzeEvents(df_fjparticles, hDict, outputDir, fileFormat)
 
   # Plot histograms
   print('Plot histograms...')
   plotHistograms(hDict, outputDir, fileFormat)
 
   print('--- %s seconds ---' % (time.time() - start_time))
+
+#---------------------------------------------------------------
+def get_fjparticles(df_tracks):
+  
+  fj_particles = []
+  for index, row in df_tracks.iterrows(): # Not sure how to do this without iterating...
+    pt = row['ParticlePt']
+    eta = row['ParticleEta']
+    phi = row['ParticlePhi']
+    px = pt * math.cos(phi)
+    py = pt * math.sin(phi)
+    pz = pt * math.sinh(eta)
+    e = pt
+    fj_particles.append(fj.PseudoJet(px, py, pz, e))
+  
+  return fj_particles
 
 #---------------------------------------------------------------
 def initializeHistograms():
@@ -99,11 +131,7 @@ def initializeHistograms():
   return hDict
 
 #---------------------------------------------------------------
-def findJets(df_tracks, hDict, outputDir, fileFormat):
-  
-  if debugLevel > 1:
-    print(df_tracks.dtypes)
-    print(df_tracks)
+def analyzeEvents(df_fjparticles, hDict, outputDir, fileFormat):
   
   fj.ClusterSequence.print_banner()
   print()
@@ -120,49 +148,33 @@ def findJets(df_tracks, hDict, outputDir, fileFormat):
   zcut = 0.1
   sd = rt.SoftDrop(beta, zcut, jetR)
   print('SoftDrop groomer is: {}'.format(sd.description()));
-  
-  # Loop through events
-  # We would like to perform a function to each group (namely jet finding, and the filling a histogram)
-  for name, group in df_tracks:
-    if debugLevel > 1:
-      print(name)
-      print(group)
-    
-    # Loop through particles in the event, and create a list of PseudoJets
-    fj_particles = []
-    for index, row in group.iterrows():
 
-      pt = row['ParticlePt']
-      eta = row['ParticleEta']
-      phi = row['ParticlePhi']
-      
-      px = pt * math.cos(phi)
-      py = pt * math.sin(phi)
-      pz = pt * math.sinh(eta)
-      e = pt
-
-      fj_particles.append( fj.PseudoJet(px, py, pz, e) )
-
-    # Do jet finding
-    cs = fj.ClusterSequence(fj_particles, jet_def)
-    jets = fj.sorted_by_pt(cs.inclusive_jets())
-    jets_accepted = jet_selector(jets)
-        
-    fillHistograms(hDict, jets_accepted)
-
-    # Loop through jets and perform SoftDrop grooming
-    jets_sd = []
-    for jet in jets_accepted:
-      jets_sd.append(sd.result(jet))
-      
-    fillSoftDropHistograms(hDict, jets_sd)
+  # Use list comprehension to do jet-finding and fill histograms
+  result = [analyzeJets(fj_particles, jet_def, jet_selector, sd, hDict) for fj_particles in df_fjparticles]
 
 #---------------------------------------------------------------
-def fillHistograms(hDict, jets_accepted):
+def analyzeJets(fj_particles, jet_def, jet_selector, sd, hDict):
+  
+  # Do jet finding
+  cs = fj.ClusterSequence(fj_particles, jet_def)
+  jets = fj.sorted_by_pt(cs.inclusive_jets())
+  jets_accepted = jet_selector(jets)
+
+  fillJetHistograms(hDict, jets_accepted)
+
+  # Loop through jets and perform SoftDrop grooming
+  jets_sd = []
+  for jet in jets_accepted:
+    jets_sd.append(sd.result(jet))
+
+  fillSoftDropHistograms(hDict, jets_sd)
+
+#---------------------------------------------------------------
+def fillJetHistograms(hDict, jets_accepted):
 
   # Loop through jets, and fill histograms
   for jet in jets_accepted:
-    if debugLevel > 0:
+    if debugLevel > 1:
       print('jet:')
       print(jet)
       
@@ -231,31 +243,32 @@ def plotHist(h, outputFilename, drawOptions = "", setLogy = False, setLogz = Fal
 #----------------------------------------------------------------------
 if __name__ == '__main__':
   # Define arguments
-  parser = argparse.ArgumentParser(description="Plot analysis histograms")
-  parser.add_argument("-f", "--inputFileTrack", action="store",
-                      type=str, metavar="inputFileTrack",
-                      default="AnalysisResultsTrackMergedIndex0.pkl",
-                      help="Path of pickled AnalysisResults file for track tree")
-  parser.add_argument("-o", "--outputDir", action="store",
-                      type=str, metavar="outputDir",
-                      default="./myTestFigures",
-                      help="Output directory for QA plots to be written to")
-  parser.add_argument("-i", "--imageFormat", action="store",
-                      type=str, metavar="imageFormat",
-                      default=".pdf",
-                      help="Image format to save plots in, e.g. \".pdf\" or \".png\"")
+  parser = argparse.ArgumentParser(description='Plot analysis histograms')
+  parser.add_argument('-f', '--inputFileTrack', action='store',
+                      type=str, metavar='inputFileTrack',
+                      default='AnalysisResultsTrackMergedIndex0.pkl',
+                      help='Path of pickled AnalysisResults file for track tree')
+  parser.add_argument('-o', '--outputDir', action='store',
+                      type=str, metavar='outputDir',
+                      default='./myTestFigures',
+                      help='Output directory for QA plots to be written to')
+  parser.add_argument('-i', '--imageFormat', action='store',
+                      type=str, metavar='imageFormat',
+                      default='.pdf',
+                      help='Image format to save plots in, e.g. \'.pdf\' or \'.png\'')
   
   # Parse the arguments
   args = parser.parse_args()
   
-  print("Configuring...")
-  print("inputFileTrack: \"{0}\"".format(args.inputFileTrack))
-  print("ouputDir: \"{0}\"".format(args.outputDir))
-  print("imageFormat: \"{0}\"".format(args.imageFormat))
+  print('Configuring...')
+  print('inputFileTrack: \'{0}\''.format(args.inputFileTrack))
+  print('ouputDir: \'{0}\"'.format(args.outputDir))
+  print('imageFormat: \'{0}\''.format(args.imageFormat))
+  print('----------------------------------------------------------------')
   
   # If invalid inputFile is given, exit
   if not os.path.exists(args.inputFileTrack):
-    print("File \"{0}\" does not exist! Exiting!".format(args.inputFileTrack))
+    print('File \"{0}\" does not exist! Exiting!'.format(args.inputFileTrack))
     sys.exit(0)
 
   plotJetDataframe(inputFileTrack = args.inputFileTrack, outputDir = args.outputDir, fileFormat = args.imageFormat)
