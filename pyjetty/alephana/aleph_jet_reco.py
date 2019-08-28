724cd32828b122c1eb00b54d3b0d0c455a8a7ce5
matplo <ploskon@gmail.com>
2019-08-28 21:27:31 +0200

    fjevent

---
+++ pyjetty/alephana/aleph_jet_reco.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import aleph
+import fastjet as fj
+import fjext
+import fjcontrib
+from tqdm import tqdm
+import sys
+import numpy as nd
+import pandas as pd
+import time
+import aleph_utils
+import joblib
+from pyjetty.fjutils.fjevent import *
+import os
+
+
+def main():
+	aleph_file="/Volumes/two/data/aleph/LEP1Data1992_recons_aftercut-001.aleph"
+	if len(sys.argv) > 1:
+		aleph_file = sys.argv[1]
+	if not os.path.exists(aleph_file):
+		print('[error] {} file does not exists'.format(aleph_file))
+		return 0
+
+	# aleph.dump(aleph_file, 2, False);
+	# aleph.dump(aleph_file, -1, True);
+
+	# print the banner first
+	fj.ClusterSequence.print_banner()
+	print()
+
+	all_jets = []
+	reader = aleph.Reader(aleph_file)
+	nev = aleph_utils.get_n_events(aleph_file)
+	pbar = tqdm(total=nev)
+	partSelector = KineSelectorFactory(absetamax=2)
+	jetSelector = KineSelectorFactory(absetamax=2, ptmin=10)
+
+	all_jets = []
+	while reader.read_next_event():
+		e = reader.get_event()
+		vparts = e.get_particles_vdoubles()
+		df = pd.DataFrame(vparts, columns=aleph.Particle.descr())
+		parts = fjext.vectorize_px_py_pz_e(df['px'].values, df['py'].values, df['pz'].values, df['e'].values)
+		fjev = FJEvent(	particles=parts, id=pbar.n, R=0.4, algorithm=fj.antikt_algorithm,
+						jet_selector=jetSelector,
+						particle_selector=partSelector)
+		fjev.run_jet_finder_csaa()
+		inclusive_jets = fjev.inclusive_jets
+		njets = len(inclusive_jets)
+		if njets > 0:
+			# print("njets = {}".format(njets))
+			all_jets.extend([j.pt() for j in inclusive_jets])
+		pbar.update()
+		if pbar.n > 1000:
+			break;
+	pbar.close()
+
+	joblib.dump(all_jets, 'test_jets.joblib')
+
+if __name__ == '__main__':
+	main()
---
+++ pyjetty/fjutils/fjevent.py
@@ -0,0 +1,73 @@
+import fastjet as fj
+import pandas as pd
+
+class KineSelectorFactory(object):
+	def __init__(self, **kwargs):
+		self.ptmin = 0.0
+		self.ptmax = +1.e6
+		self.etamin = -1.e6
+		self.etamax = +1.e6
+		self.absetamax = None
+		for key, value in kwargs.items():
+			if key == "ptmin": self.ptmin = value
+			if key == "ptmax": self.ptmax = value
+			if key == "etamin": self.etamin = value
+			if key == "etamax": self.etamax = value
+			if key == "absetamax": self.absetamax = value
+		if self.absetamax is None:
+			if abs(self.etamin) == self.etamax:
+				self.absetamax = self.etamax
+		if self.absetamax:
+			self.selector = fj.SelectorPtRange(self.ptmin, self.ptmax) & fj.SelectorAbsEtaMax(self.absetamax)
+		else:
+			self.selector = fj.SelectorPtRange(self.ptmin, self.ptmax) & fj.SelectorEtaMin(self.etamin) & fj.SelectorEtaMax(self.etamax)
+
+
+class FJEvent(object):
+	def __init__(self, **kwargs):
+		# eventid = -1, R=0.4, algorithm=fj.antikt_algorithm, name = None):
+		self.particles = []
+		self.R = 0.4
+		self.algorithm = fj.antikt_algorithm
+		self.csaa = None
+		self.jet_def = None
+		self.jet_area_def = None
+		self.cs = None
+		self.csaa = None
+		self.particle_selector = None
+		self.jet_selector = None
+		self.name = "FJEvent"
+		self.id = -1
+		for key, value in kwargs.items():
+			if key == "R": self.R = value
+			if key == "algorithm": self.algorithm = value
+			if key == "particle_selector": self.particle_selector = value
+			if key == "jet_selector": self.jet_selector = value
+			if key == "name": self.name = value
+			if key == "id": self.id = value
+			if key == "particles": self.particles = value
+		if self.name is None:
+			self.name = '{}_{}'.format(self.R, self.algorithm)
+
+	def run_jet_finder_csaa(self):
+		self.jet_def = fj.JetDefinition(self.algorithm, self.R)
+		self.jet_area_def = fj.AreaDefinition(fj.active_area, fj.GhostedAreaSpec(self.particle_selector.absetamax))
+		self.csaa = fj.ClusterSequenceArea(self.particle_selector.selector(self.particles), self.jet_def, self.jet_area_def)
+		self.inclusive_jets = fj.sorted_by_pt(self.csaa.inclusive_jets())
+		if self.jet_selector:
+			self.inclusive_jets = self.jet_selector.selector(self.inclusive_jets)
+
+	def leading_pt_particle(self, ptmin):
+		return fj.sorted_by_pt(self.particles)[0]
+
+	def leading_particle_select(self, selector):
+		return fj.sorted_by_pt(selector(self.particles))[0]
+
+	def leading_pt_jet(self, ptmin):
+		return fj.sorted_by_pt(self.inclusive_jets)[0]
+
+	def leading_jet_select(self, selector):
+		return fj.sorted_by_pt(selector(self.inclusive_jets))[0]
+
+	# implement background subtraction
+	# implement the panda output
