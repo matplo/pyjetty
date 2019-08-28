166c2b3d849a31b5c2ab23914d414da61590373e
matplo <ploskon@gmail.com>
2019-08-28 16:33:01 +0200

    major move to have 'namespacing for pyjetty'

--- alephana/run_1995.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-files=$(find "$HOME/data/aleph/LEP1/1995" -name "*.aleph")
-time parallel --bar ./invariant_mass.py -i {} -n -1 --oxo ::: ${files}
--- rootutils/fileutils.py
+++
@@ -1,6 +0,0 @@
-import uproot
-
-def list_trees_dict(fname):
-	file = uproot.open(fname)
-	all_ttrees = dict(file.allitems(filterclass=lambda cls: issubclass(cls, uproot.tree.TTreeMethods)))
-	return all_ttrees
