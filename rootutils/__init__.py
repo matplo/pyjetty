d06e02210ac9cadd8a40224bb50265d1b2d044be
matplo <ploskon@gmail.com>
2019-08-28 02:10:23 +0200

    utils

--- rootutils/__init__.py
+++ rootutils/__init__.py
@@ -1,2 +1,3 @@
 from .quiet import *
 from .treeutils import *
+from .fileutils import *
---
+++ rootutils/list_trees.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+import os
+import argparse
+import rootutils
+
+
+def main():
+	parser = argparse.ArgumentParser(description='list trees in a file', prog=os.path.basename(__file__))
+	parser.add_argument('-f', '--fname', help='path to a root file', default=None, type=str, required=True)
+	parser.add_argument('-b', '--branches', help='list also branches', default=None, required=False, action="store_true")
+	args = parser.parse_args()	
+	with rootutils.QuietWarning():
+		tu = rootutils.list_trees_dict(args.fname)
+	for t in tu:
+		print('Tree name: {}'.format(t.decode("utf-8")))
+		if args.branches:
+			_path = os.path.join(args.fname, t.decode("utf-8"))
+			with rootutils.QuietWarning():
+				tu = rootutils.TreeUtils(_path)
+				bnames = tu.bnames
+			for b in bnames:
+				print ('  - {}'.format(b))
+
+
+if __name__ == '__main__':
+	main()
