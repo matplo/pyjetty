92211922194439a5906fd93e2df533a481d7a98e
James Mulligan <james.mulligan@berkeley.edu>
2019-09-05 23:22:36 -0700

    WIP to add thermal background

--- pyjetty/jetscape/analyze_jetscape.py
+++ pyjetty/jetscape/analyze_jetscape.py
@@ -10,10 +10,12 @@ import tqdm
 import argparse
 import os
 import numpy as np
+import pandas
 import array 
 
 import pyhepmc_ng
 import ROOT
+import pu14_reader
 
 # Prevent ROOT from stealing focus when plotting
 ROOT.gROOT.SetBatch(True)
@@ -29,7 +31,10 @@ def main():
   args = parser.parse_args()	
 
   plot_hadrons = False
-
+  plot_final_state_partons = True
+  plot_nth_partons = False
+  include_thermal_background = False
+  
   # Initialize histogram dictionary
   hDict = initializeHistograms()
   
@@ -42,17 +47,22 @@ def main():
   # jet finder
   fj.ClusterSequence.print_banner()
   print()
-  jetR = 1.
+  jetR = 0.4
   jet_def = fj.JetDefinition(fj.antikt_algorithm, jetR)
   jet_selector = fj.SelectorPtMin(50.0) & fj.SelectorAbsEtaMax(2)
 
   # Jet re-clustering definition, for primary Lund plane
   jet_def_lund = fj.JetDefinition(fj.cambridge_algorithm, jetR)
   lund_gen = fjcontrib.LundGenerator(jet_def_lund)
+
+  # Load thermal background: array of dataframes, one per event
+  if include_thermal_background:
+    thermal_background_array = get_thermal_background()
   
   # Loop through events
   all_jets_hadron = []
   all_jets_parton = []
+  kt_shower_list = []
   event_hepmc = pyhepmc_ng.GenEvent()
   pbar = tqdm.tqdm(range(args.nev))
   while not input_hepmc.failed():
@@ -67,25 +77,36 @@ def main():
       hadrons = get_hadrons(event_hepmc)
       jets_hadron = find_jets(jet_def, jet_selector, hadrons)
       all_jets_hadron.extend(jets_hadron)
-    
-    #partons = get_final_partons(event_hepmc, hDict)
+    if plot_final_state_partons:
+      parton_list = get_final_partons(event_hepmc, hDict)
+    elif plot_nth_partons:
+      # Get the first parton in the shower (it is apparently the child of the initiating parton)
+      first_parton = []
+      first_parton.append(get_first_parton(event_hepmc, hDict).children[0])
+      #print('-----------------------------')
+      #print('First parton E: {}'.format(first_parton[0].momentum.e))
 
-    # Get the first parton in the shower (it is apparently the child of the initiating parton)
-    first_parton = []
-    first_parton.append(get_first_parton(event_hepmc, hDict).children[0])
-    #print('-----------------------------')
-    #print('First parton E: {}'.format(first_parton[0].momentum.e))
-
-    n = 3
-    parton_list = get_nth_partons(first_parton, n)
-    #print('Total number of partons: {}'.format(len(parton_list)))
-
+      n = 3
+      parton_list = get_nth_partons(first_parton, n)
+      #print('Total number of partons: {}'.format(len(parton_list)))      
+      
     fj_particles = []
     for parton in parton_list:
       fj_particles.append(fj.PseudoJet(parton.momentum.px, parton.momentum.py, parton.momentum.pz, parton.momentum.e))
+
+    # Append thermal background
+    if include_thermal_background:
+      thermal_background_df = thermal_background_array[0]
+      thermal_background_fjparticles = get_fjparticles(thermal_background_df)
+      fj_particles.extend(thermal_background_fjparticles)
+
     jets_parton = find_jets(jet_def, jet_selector, fj_particles)
     all_jets_parton.extend(jets_parton)
     
+    # Analyze shower history
+    max_kt = get_max_kt_shower(event_hepmc, hDict)
+    kt_shower_list.append(max_kt)
+
     pbar.update()
 
     if pbar.n >= args.nev:
@@ -114,9 +135,78 @@ def main():
   lunds_parton = [lund_gen.result(jet) for jet in all_jets_parton]
   [fill_lund_histogram(hDict, "hLundParton", splitting_list) for splitting_list in lunds_parton]
   hDict['hLundParton'].Scale(1./n_jets_parton, "width")
+
+  
+  for splitting_list in lunds_parton:
+    max_kt_recluster = 0
+    for split in splitting_list:
+      kt = split.kt()
+      if kt > max_kt_recluster:
+        max_kt_recluster = kt
+    hDict['hMaxKT_2D'].Fill(np.log(max_kt_recluster), np.log(kt))
+
   
   plot_histograms(hDict)
 
+#---------------------------------------------------------------
+def get_max_kt_shower(event_hepmc, hDict):
+
+  # Loop over all vertices in the HepMC graph, and compute kt for those which are 1-->2 splittings
+  max_kt = 0
+  for vertex in event_hepmc.vertices:
+    parent = vertex.particles_in
+    children = vertex.particles_out
+    
+    # Check if we have a splitting 
+    if len(parent) == 1 and len(children) == 2:
+
+      soft_child = None
+      if children[0].momentum.pt() < children[1].momentum.pt():
+        soft_child = children[0]
+      else:
+        soft_child = children[1]
+
+      pt_parent = parent[0].momentum.pt()
+      pt_child = soft_child.momentum.pt()
+      theta_parent = parent[0].momentum.theta()
+      theta_child = soft_child.momentum.theta()
+      phi_parent = parent[0].momentum.phi()
+      phi_child = soft_child.momentum.phi()
+        
+      z = pt_child / pt_parent
+      delta_R = np.sqrt( (theta_parent - theta_child)**2 + (phi_parent - phi_child)**2 )
+      kt = delta_R * pt_child
+      
+      if kt > max_kt:
+        max_kt = kt
+
+  hDict['hMaxKT'].Fill(np.log(max_kt))
+  return max_kt
+      
+#---------------------------------------------------------------
+# Use swig'd function to create a vector of fastjet::PseudoJets from numpy arrays of px,py,pz,e
+def get_thermal_background():
+
+  # Get thermal particle four-vectors from PU14 file
+  file = '/lustre/emmi/emmi05/thermal/Mult1700/ThermalEventsMult1700PtAv0.90_0.pu14'
+  n_particles = 1700
+  print('\n Loading PU14 background with multiplicity {} from: {}'.format(n_particles, file))
+  df_array = pu14_reader.read_file(file, n_particles, n_events_max=10)
+  print('Done.')
+  return df_array
+
+#---------------------------------------------------------------
+# Get fastjet particles from dataframe of four-vectors
+def get_fjparticles(df):
+
+  px_array = df['px'].values
+  py_array = df['py'].values
+  pz_array = df['pz'].values
+  m_array = df['m'].values
+  fj_particles = fjext.vectorize_px_py_pz_m(px_array, py_array, pz_array, m_array)
+
+  return fj_particles
+  
 #---------------------------------------------------------------
 # Get a list of the children of a given parton.
 # If the parton has no children, return the parton itself.
@@ -181,16 +271,22 @@ def initializeHistograms():
   hVertices.GetXaxis().SetTitle('n_particles')
   hDict['hVertices'] = hVertices
 
-  hLundHadron = ROOT.TH2D("hLundHadron", "hLundHadron", 80, 0, 8, 80, -3, 6)
+  hLundHadron = ROOT.TH2D("hLundHadron", "hLundHadron", 80, 0, 8, 60, 0, 6)
   hLundHadron.GetXaxis().SetTitle('ln(1/#Delta R)')
   hLundHadron.GetYaxis().SetTitle('ln(k_{T})')
   hDict['hLundHadron'] = hLundHadron
 
-  hLundParton = ROOT.TH2D("hLundParton", "hLundParton", 80, 0, 8, 80, -3, 6)
+  hLundParton = ROOT.TH2D("hLundParton", "hLundParton", 80, 0, 8, 60, 0, 6)
   hLundParton.GetXaxis().SetTitle('ln(1/#Delta R)')
   hLundParton.GetYaxis().SetTitle('ln(k_{T})')
   hDict['hLundParton'] = hLundParton
 
+  #hMaxKT = ROOT.TH2D("hMaxKT", "hMaxKT", 60, 0, 6, 60, 0, 6)
+  hMaxKT = ROOT.TH1D("hMaxKT", "hMaxKT", 60, 0, 6)
+  hMaxKT.GetXaxis().SetTitle('ln[max(k_{T})] shower')
+  #hMaxKT.GetYaxis().SetTitle('ln[max(k_{T})] re-cluster')
+  hDict['hMaxKT'] = hMaxKT
+  
   for key, val in hDict.items():
     val.Sumw2()
   
@@ -215,7 +311,6 @@ def get_hadrons(hepmc_event):
 #--------------------------------------------------------------
 def get_final_partons(hepmc_event, hDict):
 
-  fjparts = []
   partons = []
   n_vertices = len(hepmc_event.vertices)
   n_particles = len(hepmc_event.particles)
@@ -242,11 +337,7 @@ def get_final_partons(hepmc_event, hDict):
     if is_final_parton:
       partons.append(particle)
 
-  for parton in partons:
-    psj = fj.PseudoJet(parton.momentum.px, parton.momentum.py, parton.momentum.pz, parton.momentum.e)
-    fjparts.append(psj)
-
-  return fjparts
+  return partons
 
 #--------------------------------------------------------------
 def get_first_parton(hepmc_event, hDict):
