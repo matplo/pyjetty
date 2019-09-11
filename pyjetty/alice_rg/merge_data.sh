46fb20de9c31f9966d3c5e9ee84f2288181d56e6
James Mulligan <james.mulligan@berkeley.edu>
2019-09-10 17:35:52 -0700

    add script to merge data output

---
+++ pyjetty/alice_rg/merge_data.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+# Script to merge output ROOT files
+
+JOB_ID=7156
+
+# First, merge all output files on each hiccup
+HICCUPS="6 7 8 9 10 12 13"
+for HICCUP in $HICCUPS
+do
+  FILE_DIR="/remote_storage/hiccup$HICCUP/u/alice/AnalysisResults/$JOB_ID"
+  FILES=$( find "$FILE_DIR" -name "*.root" )
+
+  mkdir -p $FILE_DIR/Stage1
+  hadd -f $FILE_DIR/Stage1/AnalysisResults_hiccup$HICCUP.root $FILES
+
+done
+
+# Then, merge each hiccup output file together into a final output file
+OUTPUT_DIR="/remote_storage/hiccup6/u/alice/AnalysisResults/$JOB_ID"
+hadd -f $OUTPUT_DIR/AnalysisResultsFinal.root /remote_storage/hiccup*/u/alice/AnalysisResults/$JOB_ID/Stage1/*.root
