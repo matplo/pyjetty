d4274f4e6ae0ee293783bb0d02f779739ee5d1bc
James Mulligan <james.mulligan@berkeley.edu>
2019-09-10 23:16:20 -0700

    fix output path

--- pyjetty/alice_rg/merge_mc.sh
+++ pyjetty/alice_rg/merge_mc.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 #
 # Script to merge output ROOT files
+JOB_ID=7467
 OUTPUT_DIR="/remote_storage/hiccup6/u/alice/AnalysisResults/$JOB_ID"
 
 # Merge all output files from each pt-hat bin
 NBINS=20
 for BIN in $(seq 1 $NBINS);
 do
-  FILES=$( find /rstorage/u/alice/AnalysisResults/7467/LHC18b8/146/child_*/TrainOutput/*/$BIN -name "*.root" )
+  FILES=$( find /rstorage/u/alice/AnalysisResults/$JOB_ID/LHC18b8/146/child_*/TrainOutput/*/$BIN -name "*.root" )
 
-  mkdir -p $OUTPUT_DIR/Stage1
-  hadd -f $OUTPUT_DIR/Stage1/AnalysisResults$BIN.root $FILES
+  mkdir -p $OUTPUT_DIR/Stage1/$BIN
+  hadd -f $OUTPUT_DIR/Stage1/$BIN/AnalysisResults.root $FILES
 
 done
