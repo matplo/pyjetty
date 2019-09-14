4ac5f4f7fc01e4211125bd5f54c13b6f8f6077d8
James Mulligan <james.mulligan@berkeley.edu>
2019-09-13 23:34:03 -0700

    change module loading to latest build strategy

--- pyjetty/alice_rg/process_rg_LHC18b8.sh
+++ pyjetty/alice_rg/process_rg_LHC18b8.sh
@@ -28,9 +28,9 @@ OUTPUT_DIR="/storage/u/alice/$OUTPUT_PREFIX/$OUTPUT_SUFFIX"
 
 # Load modules
 module use /software/users/james/heppy/modules
-module load heppy/main_python3
+module load heppy/main_python
 module use /software/users/james/pyjetty/modules
-module load pyjetty/main_python3
+module load pyjetty/main_python
 module list
 
 # Run python script via pipenv
