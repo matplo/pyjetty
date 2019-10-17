d02c6ba3783a7cbfaef9e3597faac063e3ae7754
Ezra Lesser <ezralesser@gmail.com>
2019-10-17 01:12:47 -0700

    trying to debug scripts

--- pyjetty/alice_analysis/slurm/sbatch/ang/ang_LHC17pq.sh
+++ pyjetty/alice_analysis/slurm/sbatch/ang/ang_LHC17pq.sh
@@ -7,7 +7,7 @@
 # Take three command line arguments -- (1) input file path, (2) job ID, (3) task ID
 if [ "$1" != "" ]; then
   INPUT_FILE=$1
-  #echo "Input file: $INPUT_FILE"
+  echo "Input file: $INPUT_FILE"
 else
   echo "Wrong command line arguments"
 fi
@@ -31,7 +31,7 @@ OUTPUT_PREFIX="AnalysisResults/ang/$JOB_ID"
 # Note: depends on file structure of input file -- need to edit appropriately for each dataset
 OUTPUT_SUFFIX=$(echo $INPUT_FILE | cut -d/ -f5-8)
 #echo $OUTPUT_SUFFIX
-OUTPUT_DIR="/storage/u/alice/$OUTPUT_PREFIX/$OUTPUT_SUFFIX"
+OUTPUT_DIR="/rstorage/u/alice/$OUTPUT_PREFIX/$OUTPUT_SUFFIX"
 #echo "Output dir: $OUTPUT_DIR"
 
 # Load modules
@@ -46,4 +46,4 @@ cd /home/ezra/pyjetty/pyjetty/alice_analysis
 pipenv run python process/user/ang_pp/ang_data.py -c config/angularity.yaml -f $INPUT_FILE -o $OUTPUT_DIR
 
 # Move stdout to appropriate folder
-mv /storage/u/alice/AnalysisResults/slurm-${JOB_ID}_${TASK_ID}.out /storage/u/alice/AnalysisResults/${JOB_ID}
+mv /rstorage/u/alice/AnalysisResults/ang/slurm-${JOB_ID}_${TASK_ID}.out /rstorage/u/alice/AnalysisResults/ang/${JOB_ID}
--- pyjetty/alice_analysis/slurm/sbatch/ang/ang_slurm_LHC17pq.sh
+++ pyjetty/alice_analysis/slurm/sbatch/ang/ang_slurm_LHC17pq.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-#SBATCH --job-name=rgtest
+#SBATCH --job-name="angtest"
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1
 #SBATCH --partition=std
 #SBATCH --time=24:00:00
 #SBATCH --array=1-100
-#SBATCH --output=/storage/u/alice/AnalysisResults/ang/slurm-%A_%a.out
+#SBATCH --mail-user=elesser@berkeley.edu --mail-type=ALL
+#SBATCH --output=/rstorage/u/alice/AnalysisResults/ang/slurm-%A_%a.out
 
 FILE_PATHS='/rstorage/u/alice/LHC17pq/145/files.txt'
 NFILES=$(wc -l < $FILE_PATHS)
@@ -15,7 +16,7 @@ echo "N files to process: ${NFILES}"
 FILES_PER_JOB=$(( $NFILES / 100 + 1 ))
 echo "Files per job: $FILES_PER_JOB"
 
-STOP=$(( SLURM_ARRAY_TASK_ID*FILES_PER_JOB ))
+STOP=$(( (SLURM_ARRAY_TASK_ID + 1) * FILES_PER_JOB ))
 START=$(( $STOP - $(( $FILES_PER_JOB - 1 )) ))
 
 if (( $STOP > $NFILES ))
@@ -25,10 +26,8 @@ fi
 
 echo "START=$START"
 echo "STOP=$STOP"
-
-OUTPUT_PREFIX="AnalysisResults/$SLURM_ARRAY_JOB_ID"
 for (( JOB_N = $START; JOB_N <= $STOP; JOB_N++ ))
 do
   FILE=$(sed -n "$JOB_N"p $FILE_PATHS)
-  srun ang_LHC17pq.sh $FILE $SLURM_ARRAY_JOB_ID $SLURM_ARRAY_TASK_ID
+  srun ang_LHC17pq.sh $FILE $((SLURM_ARRAY_JOB_ID)) $((SLURM_ARRAY_TASK_ID + 1))
 done
