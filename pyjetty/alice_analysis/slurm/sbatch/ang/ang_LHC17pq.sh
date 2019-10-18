c41ec15f03fee3c8792cacc1539f1b7de1aa3c6a
Ezra Lesser <ezralesser@gmail.com>
2019-10-18 13:36:15 -0700

    Working ang data script

--- pyjetty/alice_analysis/slurm/sbatch/ang/ang_LHC17pq.sh
+++ pyjetty/alice_analysis/slurm/sbatch/ang/ang_LHC17pq.sh
@@ -31,8 +31,9 @@ OUTPUT_PREFIX="AnalysisResults/ang/$JOB_ID"
 # Note: depends on file structure of input file -- need to edit appropriately for each dataset
 OUTPUT_SUFFIX=$(echo $INPUT_FILE | cut -d/ -f5-8)
 #echo $OUTPUT_SUFFIX
-OUTPUT_DIR="/rstorage/u/alice/$OUTPUT_PREFIX/$OUTPUT_SUFFIX"
-#echo "Output dir: $OUTPUT_DIR"
+OUTPUT_DIR="/storage/u/alice/$OUTPUT_PREFIX/$OUTPUT_SUFFIX/"
+mkdir -p $OUTPUT_DIR
+echo "Output dir: $OUTPUT_DIR"
 
 # Load modules
 module use /home/ezra/heppy/modules
@@ -42,8 +43,8 @@ module load pyjetty/main_python
 module list
 
 # Run python script via pipenv
-cd /home/ezra/pyjetty/pyjetty/alice_analysis
-pipenv run python process/user/ang_pp/ang_data.py -c config/angularity.yaml -f $INPUT_FILE -o $OUTPUT_DIR
+cd /home/ezra/pyjetty/pyjetty/alice_analysis/
+python process/user/ang_pp/ang_data.py -c config/angularity.yaml -f $INPUT_FILE -o $OUTPUT_DIR
 
 # Move stdout to appropriate folder
-mv /rstorage/u/alice/AnalysisResults/ang/slurm-${JOB_ID}_${TASK_ID}.out /rstorage/u/alice/AnalysisResults/ang/${JOB_ID}
+mv /storage/u/alice/AnalysisResults/ang/slurm-${JOB_ID}_${TASK_ID}.out /storage/u/alice/AnalysisResults/ang/${JOB_ID}
