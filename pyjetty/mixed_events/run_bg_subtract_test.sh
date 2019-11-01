0df112766bf074ad8ca8579390b9da1ca8e6a67a
matplo <ploskon@gmail.com>
2019-11-01 16:44:18 -0700

    t formation modifs

--- pyjetty/mixed_events/run_bg_subtract_test.sh
+++ pyjetty/mixed_events/run_bg_subtract_test.sh
@@ -1,1 +1,2 @@
-./make_background_estimator.py --test --data ~/data/alice/LHC18qr/local_PbPb_file_list.txt --output wbsub.root --nev 10000 --overwrite --benchmark
+#./make_background_estimator.py --test --data ~/data/alice/LHC18qr/local_PbPb_file_list.txt --output wbsub.root --nev 10000 --overwrite --benchmark
+./make_background_estimator.py --test --output wbsub_thm.root --nev 1000 --overwrite --benchmark
