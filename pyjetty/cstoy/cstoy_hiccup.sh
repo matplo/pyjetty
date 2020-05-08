bf90a877ef5d6934490475749db6535f15ca685a
matplo <ploskon@gmail.com>
2020-05-07 21:44:31 -0700

    fix according to the new build

--- pyjetty/cstoy/cstoy_hiccup.sh
+++ pyjetty/cstoy/cstoy_hiccup.sh
@@ -11,10 +11,11 @@ mkdir -p /storage/u/ploskon/cstoy
 cd /storage/u/ploskon/cstoy
 
 source scl_source enable devtoolset-7
-module use /home/software/users/ploskon/heppy-devtools7/modules
-module load heppy/main_python	
+#module use /home/software/users/ploskon/heppy-devtools7/modules
+#module load heppy/main_python	
 module use /home/software/users/ploskon/pyjetty/modules
-module load pyjetty/main_python
+#module load pyjetty/main_python
+module load pyjetty/1.0
 
 set -x
 /home/software/users/ploskon/heppy/scripts/pipenv_heppy.sh run "$PYJETTYDIR/pyjetty/cstoy/cstoy.py --nev ${3} --noue --pthatmin 100 --alpha ${1} --dRmax ${2}"
