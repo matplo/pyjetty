a33460542ea66fe6c5e8893fa1d706bbb88ab716
James Mulligan <james.mulligan@berkeley.edu>
2020-05-07 23:29:25 -0700

    adaptations to ev_id_ext

--- pyjetty/alice_analysis/process/user/james/process_data.py
+++ pyjetty/alice_analysis/process/user/james/process_data.py
@@ -59,7 +59,8 @@ class ProcessData(process_base.ProcessBase):
 
     # Use IO helper class to convert ROOT TTree into a SeriesGroupBy object of fastjet particles per event
     print('--- {} seconds ---'.format(time.time() - self.start_time))
-    io = process_io.ProcessIO(input_file=self.input_file, track_tree_name='tree_Particle', is_pp=self.is_pp)
+    io = process_io.ProcessIO(input_file=self.input_file, track_tree_name='tree_Particle',
+                              is_pp=self.is_pp, use_ev_id_ext=self.use_ev_id_ext)
     self.df_fjparticles = io.load_data()
     self.nEvents = len(self.df_fjparticles.index)
     self.nTracks = len(io.track_df.index)
@@ -100,6 +101,8 @@ class ProcessData(process_base.ProcessBase):
         self.is_pp = False
     else:
         self.is_pp = True
+        
+    self.use_ev_id_ext = config['use_ev_id_ext']
        
     self.observable_list = config['process_observables']
     
