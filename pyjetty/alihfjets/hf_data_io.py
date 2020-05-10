4d887b8b02e9b3c9f415fede04e4aa5608f363b8
matplo <ploskon@gmail.com>
2020-05-10 11:01:19 -0700

    write a tree; status bar; trying other cuts

--- pyjetty/alihfjets/hf_data_io.py
+++ pyjetty/alihfjets/hf_data_io.py
@@ -6,24 +6,34 @@ import fastjet as fj
 import fjext
 import os
 import tqdm
-
+# from numba import jit
+import numexpr
 
 class HFAnalysis(MPBase):
 	def __init__(self, **kwargs):
 		super(HFAnalysis, self).__init__(**kwargs)
 		self.selection = []
 		self.df_selection = None
+		self.query_strings = []
+		self.query_string = ''
+		self.callback = None
 
 	def add_selection_equal(self, what, val):
 		self.selection.append([what, val, None, 0])
+		self.query_strings.append('({} == {})'.format(what, val))
+		self.query_string = '(' + ' & '.join(self.query_strings) + ')'
 
-	def add_selection_range(self, what, minv, maxv=None):
+	def add_selection_range(self, what, minv, maxv):
 		self.selection.append([what, minv, maxv, 1])
+		self.query_strings.append('({} > {}) & ({} < {})'.format(what, minv, what, maxv))
+		self.query_string = '(' + ' & '.join(self.query_strings) + ')'
 
 	def add_selection_range_abs(self, what, val):
 		self.selection.append([what, val, None, 2])
+		self.query_strings.append('({} > {}) & ({} < {})'.format(what, -val, what, +val))
+		self.query_string = '(' + ' & '.join(self.query_strings) + ')'
 
-	def analyze(self, df):
+	def compile_selection(self, df):
 		self.df_selection = True
 		for c in self.selection:
 			if c[3] == 0:
@@ -32,7 +42,16 @@ class HFAnalysis(MPBase):
 				self.df_selection = (self.df_selection) & (df[c[0]] > c[1]) & (df[c[0]] < c[2])
 			if c[3] == 2:
 				self.df_selection = (self.df_selection) & (df[c[0]] > -c[1]) & (df[c[0]] < c[1])
-		_df = df.loc[self.df_selection]
+
+	def analyze(self, df):
+		self.compile_selection(df)
+		_df = df[self.df_selection]
+		self.analysis(_df)
+		if self.callback is not None:
+			self.callback(df['ev_id'].values[0])
+
+	def analyze_slower(self, df):
+		_df = df.query(self.query_string)
 		self.analysis(_df)
 
 	# analysis on the single data frame
@@ -91,7 +110,16 @@ class HFAnalysisIO(MPBase):
 	def execute_analyses(self):
 		[self.track_df_grouped.apply(a.analyze) for a in self.analyses]
 
+	def update_status(self, mark):
+		if mark != self.pbar2_mark:
+			self.pbar2_mark = mark
+			self.pbar2.update(1)
+
 	def execute_analyses_on_file_list(self, file_list, nfiles=0):
+		self.pbar2 = tqdm.tqdm()
+		self.pbar2_mark = None
+		for a in self.analyses:
+			a.callback = self.update_status
 		if os.path.exists(file_list):
 			with open(file_list) as f:
 				files = f.readlines()
@@ -104,6 +132,7 @@ class HFAnalysisIO(MPBase):
 				fn = f.strip('\n')
 				if self.load_file(fn):
 					self.execute_analyses()
+			self.pbar2.close()
 		else:
 			perror('file list does not exist', file_list)
 		pinfo('done.')
--- pyjetty/alihfjets/test_hfana.py
+++ pyjetty/alihfjets/test_hfana.py
@@ -3,7 +3,7 @@
 import argparse
 import os
 import hf_data_io as hfdio
-from pyjetty.mputils import perror, pinfo, pwarning
+from pyjetty.mputils import perror, pinfo, pwarning, treewriter
 import ROOT
 ROOT.gROOT.SetBatch(True)
 
@@ -13,13 +13,20 @@ class HFAnalysisInvMass(hfdio.HFAnalysis):
 		super(HFAnalysisInvMass, self).__init__(**kwargs)
 		self.fout = ROOT.TFile(self.name+'.root', 'recreate')
 		self.fout.cd()
-		self.hinvmass = ROOT.TH1F('hinvmass', 'hinvmass', 100, 1, 3)
-		self.hinvmass.Sumw2()
+		# self.hinvmass = ROOT.TH1F('hinvmass', 'hinvmass', 400, 1.5, 2.5)
+		# self.hinvmass.Sumw2()
+		# self.hinvmasspt = ROOT.TH2F('hinvmasspt', 'hinvmasspt', 400, 1.5, 2.5, 50, 2, 12)
+		# self.hinvmasspt.Sumw2()
+		self.tw = treewriter.RTreeWriter(tree_name='d0', fout=self.fout)
 
 	def analysis(self, df):
 		for index, row in df.iterrows():
-			self.hinvmass.Fill(row['inv_mass'])
-
+			# self.hinvmass.Fill(row['inv_mass'])
+			# self.hinvmasspt.Fill(row['inv_mass'], row['pt_cand'])
+			for c in df.columns:
+				self.tw.fill_branch(c, row[c])
+			self.tw.fill_tree()
+				
 	def finalize(self):
 		self.fout.Write()
 		self.fout.Close()
@@ -36,15 +43,24 @@ if __name__ == '__main__':
 	hfa = HFAnalysisInvMass(name = 'HFAnalysisInvMass')
 	hfa.add_selection_range('pt_cand', 2, 1e3)
 	hfa.add_selection_range_abs('z_vtx_reco', 10)
+	hfa.add_selection_range('pt_prong0', 1, 1e3)
+	hfa.add_selection_range('pt_prong1', 1, 1e3)
 	hfa.add_selection_range_abs('eta_cand', 0.8)
-	hfa.add_selection_range_abs('nsigTPC_Pi_0', 2)
-	hfa.add_selection_range_abs('nsigTPC_Pi_1', 2)
-	hfa.add_selection_range_abs('nsigTPC_K_0', 2)
-	hfa.add_selection_range_abs('nsigTPC_K_1', 2)
-	hfa.add_selection_range_abs('nsigTOF_Pi_0', 2)
-	hfa.add_selection_range_abs('nsigTOF_Pi_1', 2)
-	hfa.add_selection_range_abs('nsigTOF_K_0', 2)
-	hfa.add_selection_range_abs('nsigTOF_K_1', 2)
+	hfa.add_selection_range_abs('nsigTPC_Pi_0', 3)
+	hfa.add_selection_range_abs('nsigTPC_Pi_1', 3)
+	hfa.add_selection_range_abs('nsigTPC_K_0', 3)
+	hfa.add_selection_range_abs('nsigTPC_K_1', 3)
+	hfa.add_selection_range_abs('nsigTOF_Pi_0', 3)
+	hfa.add_selection_range_abs('nsigTOF_Pi_1', 3)
+	hfa.add_selection_range_abs('nsigTOF_K_0', 3)
+	hfa.add_selection_range_abs('nsigTOF_K_1', 3)
+
+	#hfa.add_selection_range('cos_p_xy', 0, 0.99)
+	#hfa.add_selection_range('d_len_xy', 0.1, 1e3)
+	#hfa.add_selection_range('dca', 0.01, 1e3)
+
+	# hfa.add_selection_range_abs('imp_par_prong0', -0.01)
+	# hfa.add_selection_range_abs('imp_par_prong1', -0.01)
 
 	hfaio.add_analysis(hfa)
 
