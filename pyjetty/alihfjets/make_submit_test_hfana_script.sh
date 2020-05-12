65549c1a00bd6a75d50200af16aa9ac7b0720a37
matplo <ploskon@gmail.com>
2020-05-12 03:32:39 -0700

    slurm on hiccup

---
+++ pyjetty/alihfjets/make_submit_test_hfana_script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cpwd=${PWD}
+
+pp5TeVfiles=/rstorage/alice/data/LHC17pq/filelist.txt
+flist=${pp5TeVfiles}
+
+dname=$(date +"%Y-%m-%d-%H-%M")
+
+flistdname=$(dirname ${flist})
+outputdir=$(basename ${flistdname})
+outputdir=/rstorage/${USER}/test_hfana/${outputdir}/${dname}
+mkdir -pv ${outputdir}
+cd ${outputdir}
+pwd
+
+cp -v ${flist} .
+split --additional-suffix=.flist -d -l 36 -a 5 ${flist}
+
+job_lists=$(find $PWD -name "*.flist")
+
+cp -v ${PYJETTY_DIR}/pyjetty/alihfjets/test_hfana.sh .
+cp -v ${PYJETTY_DIR}/pyjetty/alihfjets/test_hfana.py .
+
+executable=${PWD}/test_hfana.py
+
+submit_script=${PWD}/submit_all.sh
+rm -f ${submit_script}
+
+for fl in ${job_lists}
+do
+	echo "sbatch --chdir=${PWD} --output=${fl}.output --error=${fl}.error ${PWD}/test_hfana.sh ${executable} ${fl} ${outputdir}" | tee -a ${submit_script}
+	chmod +x ${submit_script}
+done
+
+echo "[i] created: ${submit_script}"
+
+cd ${cpwd}
---
+++ pyjetty/alihfjets/test_hfana.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=test_hfana
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1
+#SBATCH --partition=std
+#SBATCH --time=4:00:00
+
+id
+hostname
+date
+
+source scl_source enable devtoolset-7
+module use /home/software/users/${USER}/pyjetty/modules
+module avail
+module load pyjetty/1.0
+module list
+
+set -x
+executable=${1}
+flist=${2}
+outputdir=${3}
+output=${outputdir}/$(basename ${flist})
+
+if [ -e ${flist} ]; then
+	mkdir -p ${outputdir}
+	if [ -d ${outputdir} ]; then
+		cd ${outputdir}
+		if [ -x ${executable} ]; then
+			${executable} -f ${flist} -o ${output}
+		else
+			echo "[e] no executable: ${executable}"
+		fi
+	else
+		echo "[e] no outputdir: ${outputdir}"
+	fi
+else
+	echo "[e] no file list: ${flist}"
+fi
+
+date
