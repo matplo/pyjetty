f16bc4fff30250f645fe0207b2e8dcca505d20ec
James Mulligan <james.mulligan@berkeley.edu>
2020-05-20 21:33:12 -0700

    minor config updates

--- pyjetty/alice_analysis/slurm/sbatch/james/pp_ref/slurm_LHC18b8.sh
+++ pyjetty/alice_analysis/slurm/sbatch/james/pp_ref/slurm_LHC18b8.sh
@@ -4,15 +4,15 @@
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1
 #SBATCH --partition=std
 #SBATCH --time=24:00:00
-#SBATCH --array=1-140
+#SBATCH --array=1-160
 #SBATCH --output=/rstorage/alice/AnalysisResults/james/slurm-%A_%a.out
 
 FILE_PATHS='/rstorage/alice/data/LHC18b8/146/files.txt'
 NFILES=$(wc -l < $FILE_PATHS)
 echo "N files to process: ${NFILES}"
 
-# Currently we have 7 nodes * 20 cores active
-FILES_PER_JOB=$(( $NFILES / 140 + 1 ))
+# Currently we have 8 nodes * 20 cores active
+FILES_PER_JOB=$(( $NFILES / 160 + 1 ))
 echo "Files per job: $FILES_PER_JOB"
 
 STOP=$(( SLURM_ARRAY_TASK_ID*FILES_PER_JOB ))
