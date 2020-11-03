535e51dd085a7d0fa66343510cb08f30cd69adc4
Ezra Lesser <ezralesser@gmail.com>
2020-11-02 16:33:08 -0800

    Switch sbatch scripts to gen yaml file

--- pyjetty/alice_analysis/slurm/sbatch/ang/gen/herwig_ang_slurm.sh
+++ pyjetty/alice_analysis/slurm/sbatch/ang/gen/herwig_ang_slurm.sh
@@ -36,7 +36,7 @@ SEED=$(( ($CORE_IN_BIN - 1) * NEV_PER_JOB + 1111 ))
 HERWIG_SCRIPT="/home/ezra/pyjetty/pyjetty/alice_analysis/process/user/ang_pp/herwig_infiles/$BIN/LHC_5020.run"
 HERWIG_SCRIPT_MPI="/home/ezra/pyjetty/pyjetty/alice_analysis/process/user/ang_pp/herwig_infiles/$BIN/LHC_5020_MPI.run"
 PYTHON_SCRIPT="/home/ezra/pyjetty/pyjetty/alice_analysis/process/user/ang_pp/herwig_parton_hadron.py"
-CONFIG="/home/ezra/pyjetty/pyjetty/alice_analysis/config/ang/process_angularity.yaml"
+CONFIG="/home/ezra/pyjetty/pyjetty/alice_analysis/config/ang/gen_angularity.yaml"
 TEMP_OUTDIR="/storage/u/alice/AnalysisResults/ang/$SLURM_ARRAY_JOB_ID/$BIN/$CORE_IN_BIN"
 OUTDIR="/rstorage/alice/AnalysisResults/ang/$SLURM_ARRAY_JOB_ID/$BIN/$CORE_IN_BIN"
 mkdir -p $TEMP_OUTDIR
