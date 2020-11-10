73fd3e1a40f9746b0ef3e520504be3fe546fc7ef
Ezra Lesser <ezralesser@gmail.com>
2020-11-10 09:39:43 -0800

    Update processing scripts

--- pyjetty/alice_analysis/slurm/utils/ang/merge_data.sh
+++ pyjetty/alice_analysis/slurm/utils/ang/merge_data.sh
@@ -2,15 +2,10 @@
 #
 # Script to merge output ROOT files
 
-JOB_ID=59376
+JOB_ID=230386
 
-# First, merge all output files on each hiccup
 FILE_DIR="/rstorage/alice/AnalysisResults/ang/$JOB_ID"
 FILES=$( find "$FILE_DIR" -name "*.root" )
 
-mkdir -p $FILE_DIR/Stage1
-hadd -f $FILE_DIR/Stage1/AnalysisResults_hiccup$HICCUP.root $FILES
-
-# Then, merge each hiccup output file together into a final output file
-OUTPUT_DIR="/rstorage/alice/AnalysisResults/ang/$JOB_ID"
-hadd -f $OUTPUT_DIR/AnalysisResultsFinal.root /rstorage/alice/AnalysisResults/ang/$JOB_ID/Stage1/*.root
+OUTPUT_DIR=/rstorage/alice/AnalysisResults/ang/$JOB_ID
+hadd -f -j 20 $OUTPUT_DIR/AnalysisResultsFinal.root $FILES
