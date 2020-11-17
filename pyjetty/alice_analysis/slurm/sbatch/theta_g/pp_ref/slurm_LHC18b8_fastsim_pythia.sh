78df9cf21a02d53f6aa1d045b44167e219c9370e
James Mulligan <james.mulligan@berkeley.edu>
2020-11-17 09:09:08 -0800

    some path fixes for fastsim production/merging

--- pyjetty/alice_analysis/slurm/sbatch/theta_g/pp_ref/slurm_LHC18b8_fastsim_pythia.sh
+++ pyjetty/alice_analysis/slurm/sbatch/theta_g/pp_ref/slurm_LHC18b8_fastsim_pythia.sh
@@ -7,13 +7,13 @@
 #SBATCH --array=1-160
 #SBATCH --output=/rstorage/alice/AnalysisResults/james/slurm-%A_%a.out
 
-FILE_PATHS='/rstorage/generators/pythia_alice/tree_fastsim/257194/files.txt'
+FILE_PATHS='/rstorage/generators/pythia_alice/tree_fastsim/258314/files.txt'
 NFILES=$(wc -l < $FILE_PATHS)
 echo "N files to process: ${NFILES}"
 
 # Currently we have 8 nodes * 20 cores active
 FILES_PER_JOB=$(( $NFILES / 160 + 1 ))
-echo "Files per job: $FILES_PER_JOBs
+echo "Files per job: $FILES_PER_JOB"
 
 STOP=$(( SLURM_ARRAY_TASK_ID*FILES_PER_JOB ))
 START=$(( $STOP - $(( $FILES_PER_JOB - 1 )) ))
@@ -29,5 +29,5 @@ echo "STOP=$STOP"
 for (( JOB_N = $START; JOB_N <= $STOP; JOB_N++ ))
 do
   FILE=$(sed -n "$JOB_N"p $FILE_PATHS)
-  srun process_LHC18b8.sh $FILE $SLURM_ARRAY_JOB_ID $SLURM_ARRAY_TASK_ID
+  srun process_LHC18b8_fastsim_pythia.sh $FILE $SLURM_ARRAY_JOB_ID $SLURM_ARRAY_TASK_ID
 done
