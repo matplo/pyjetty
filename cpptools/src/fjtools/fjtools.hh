830486c619f568aff6a6d60fa4b0b89c3221f804
James Mulligan <james.mulligan@berkeley.edu>
2020-11-20 10:54:06 -0800

    add RooUnfold to fjtools, and work on rebinning function

--- cpptools/src/fjtools/fjtools.hh
+++ cpptools/src/fjtools/fjtools.hh
@@ -8,6 +8,10 @@ class TH2F;
 #include <vector>
 #include <fastjet/PseudoJet.hh>
 
+#ifdef USE_ROOUNFOLD
+#include <RooUnfoldResponse.h>
+#endif
+
 namespace PyJettyFJTools
 {
 	class BoltzmannBackground
@@ -60,18 +64,35 @@ namespace PyJettyFJTools
 
 	// Rebin N-dimensional THn to a new histogram with name name_thn_rebinned using provided axes
 	// WARNING: currently requires n_dim = 4
-	THnF* rebin_thn(THnF* thn, const std::string & name_thn_rebinned,
-					const unsigned int & n_dim, const int* axes_n_bins,
-					double** axes_bin_arrays, const double & prior_variation_parameter=0.,
-					bool move_underflow=false, bool do_roounfoldresponse=true);
+	THnF* rebin_thn(std::string response_file_name,
+                    THnF* thn,
+                    const std::string & name_thn_rebinned,
+                    const std::string & name_roounfold,
+					const unsigned int & n_dim,
+                    const int* axes_n_bins,
+					double** axes_bin_arrays,
+                    const std::string label="",
+                    const double & prior_variation_parameter=0.,
+                    int prior_option=1,
+					bool move_underflow=false,
+                    bool do_roounfoldresponse=true);
 
 	// Create empty THn using provided axes
 	THnF* create_empty_thn(const char* name, const unsigned int & n_dim, const char** axes_titles,
 						   const int* axes_n_bins, double** axes_bin_arrays);
 
 	// Fill empty thn_rebinned with data from thn
-	void fill_rebinned_thn(THnF* thn, THnF* thn_rebinned, const unsigned int & n_dim,
-						   const double & prior_variation_parameter=0., bool move_underflow=false);
+	void fill_rebinned_thn(std::string response_file_name, THnF* thn,
+                           THnF* thn_rebinned, const unsigned int & n_dim,
+                           bool do_roounfoldresponse=true,
+                           RooUnfoldResponse* roounfold_response=nullptr,
+						   const double & prior_variation_parameter=0.,
+                           int prior_option=1,
+                           bool move_underflow=false);
+
+    // Set scaling of prior
+    double prior_scale_factor_obs(double obs_true, double content,
+                                  double prior_variation_parameter, int option);
 
 };
 
