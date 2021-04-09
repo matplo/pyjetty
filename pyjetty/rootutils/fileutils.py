fc61770122c7e64b13c8c317a66e02157f62f546
matplo <ploskon@gmail.com>
2021-04-08 21:53:08 -0700

    fixes & uproot4

--- pyjetty/rootutils/fileutils.py
+++ pyjetty/rootutils/fileutils.py
@@ -1,6 +1,13 @@
 import uproot
 
 def list_trees_dict(fname):
-	file = uproot.open(fname)
-	all_ttrees = dict(file.allitems(filterclass=lambda cls: issubclass(cls, uproot.tree.TTreeMethods)))
-	return all_ttrees
+	if uproot.version.version_info[0] == '3':
+		file = uproot.open(fname)
+		all_ttrees = dict(file.allitems(filterclass=lambda cls: issubclass(cls, uproot.tree.TTreeMethods)))
+		return all_ttrees
+	if uproot.version.version_info[0] == '4':
+		with uproot.open(fname) as f:
+			all_ttrees = dict(f.classnames(filter_classname="TNtuple"))
+			all_ttrees.update(dict(f.classnames(filter_classname="TTree")))
+			return all_ttrees
+	return None
