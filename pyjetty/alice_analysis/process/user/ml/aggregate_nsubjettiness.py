06e8a6c8389f4eb72f227f3a8e7dcd3fc0117b98
James Mulligan <james.mulligan@berkeley.edu>
2021-07-01 18:28:16 -0700

    update aggregate for delta-pt, matched jets, four-vectors, and more

--- pyjetty/alice_analysis/process/user/ml/aggregate_nsubjettiness.py
+++ pyjetty/alice_analysis/process/user/ml/aggregate_nsubjettiness.py
@@ -11,8 +11,8 @@ import numpy as np
 def aggregate(config_file, filelist, output_dir):
 
     # List of arrays to aggregate
-    #observables = ['X_four_vectors', 'X_Nsub', 'y', 'pt', 'delta_pt']
-    observables = ['X_Nsub', 'y', 'pt', 'delta_pt']
+    observables = ['X_four_vectors', 'X_Nsub', 'y', 'jet_pt', 'delta_pt', 'jet_angularity', 'jet_mass', 'jet_theta_g', 'jet_subjet_z', 'hadron_z']
+    #observables = ['X_Nsub', 'y', 'jet_pt', 'delta_pt', 'jet_angularity', 'jet_mass', 'jet_theta_g', 'jet_subjet_z', 'hadron_z']
 
     # Read config file
     with open(config_file, 'r') as stream:
@@ -20,7 +20,7 @@ def aggregate(config_file, filelist, output_dir):
     jetR_list = config['jetR']
     jet_pt_bins = config['jet_pt_bins']
     max_distance_list = config['constituent_subtractor']['max_distance']
-    event_types = ['hard', 'combined']
+    event_types = ['hard', 'combined', 'combined_matched']
 
     # We have separate training data for:
     # - the hard-event and the combined-event
@@ -30,12 +30,13 @@ def aggregate(config_file, filelist, output_dir):
     # Create a flat dict of empty objects for each combination
     # We use keys equal to the keys in the input file
     output = {}
+    output['delta_pt_random_cone'] = np.array([])
     for event_type in event_types:
         for jetR in jetR_list:
             for jet_pt_bin in jet_pt_bins:
                 for R_max in max_distance_list:
                     for observable in observables:
-                        if event_type=='combined' or np.isclose(R_max, 0.):
+                        if accepted_setting(observables, event_type, R_max):
                             output_key = f'{observable}_{event_type}_R{jetR}_pt{jet_pt_bin}_Rmax{R_max}'
                             output[output_key] = np.array([])
     N_list = None
@@ -46,7 +47,10 @@ def aggregate(config_file, filelist, output_dir):
         files = [line.rstrip() for line in f]
         n_files = len(files)
     for i,filename in enumerate(files):
-    
+        
+        #if i > 1000:
+        #    break
+
         if i%100 == 0:
             print(f'{i}/{n_files}')
 
@@ -55,37 +59,43 @@ def aggregate(config_file, filelist, output_dir):
             if not N_list:
                 N_list = list(hdf['N_list'][:])
                 beta_list = list(hdf['beta_list'][:])
-        
+
+            delta_pt_random_cone_i = hdf['delta_pt_random_cone'][:]
+            if output['delta_pt_random_cone'].any():
+                output['delta_pt_random_cone'] = np.concatenate((output['delta_pt_random_cone'], delta_pt_random_cone_i),axis=0)
+            else:
+                output['delta_pt_random_cone'] = delta_pt_random_cone_i
+    
             for event_type in event_types:
                 for jetR in jetR_list:
                     for jet_pt_bin in jet_pt_bins:
                         for R_max in max_distance_list:
                             for observable in observables:
-
-                                if event_type=='combined' or np.isclose(R_max, 0.):
+                                if accepted_setting(observables, event_type, R_max):
                             
                                     # Get arrays from file
                                     output_key = f'{observable}_{event_type}_R{jetR}_pt{jet_pt_bin}_Rmax{R_max}'
                                     output_i = hdf[output_key][:]
-                                    
-                                    # Concatenate to master array
-                                    output_aggregated = output[output_key]
-                                    if output_aggregated.any():
-                                        output[output_key] = np.concatenate((output_aggregated, output_i),axis=0)
-                                    else:
-                                        output[output_key] = output_i
+                                    if output_i.shape[0] > 0:
 
-                                    if i%100 == 0:
-                                        if observable in ['X_Nsub']:
-                                            print(f'{output_key} -- {output_aggregated.shape}')
+                                        # Concatenate to master array
+                                        output_aggregated = output[output_key]
+                                        if output_aggregated.any():
+                                            output[output_key] = np.concatenate((output_aggregated, output_i),axis=0)
+                                        else:
+                                            output[output_key] = output_i
+
+                                        if i%100 == 0:
+                                            if observable in ['X_Nsub']:
+                                                print(f'{output_key} -- {output_aggregated.shape}')
 
     #  Shuffle data set
     for event_type in event_types:
         for jetR in jetR_list:
             for jet_pt_bin in jet_pt_bins:
                 for R_max in max_distance_list:
-                    if event_type=='combined' or np.isclose(R_max, 0.):
-                        
+                    if accepted_setting(observables, event_type, R_max):
+                    
                         output_key_y = f'y_{event_type}_R{jetR}_pt{jet_pt_bin}_Rmax{R_max}'
                         idx = np.random.permutation(len(output[output_key_y]))
                     
@@ -96,25 +106,49 @@ def aggregate(config_file, filelist, output_dir):
                                 output_aggregated = output[output_key]
                                 output[output_key] = output_aggregated[idx]
                                 print(f'shuffled {output_key}: {output[output_key].shape}')
+
+                                # Remove nan
+                                where_are_nan = np.isnan(output[output_key])
+                                output[output_key][where_are_nan] = 0.
         
     # Write jet arrays to file
-    with h5py.File(os.path.join(output_dir, 'nsubjettiness.h5'), 'w') as hf:
+    if 'X_four_vectors' in observables:
+        output_filename = 'nsubjettiness_with_four_vectors.h5'
+    else:
+        output_filename = 'nsubjettiness_without_four_vectors.h5' 
+        
+    with h5py.File(os.path.join(output_dir, output_filename), 'w') as hf:
     
         hf.create_dataset('N_list', data=np.array(N_list))
         hf.create_dataset('beta_list', data=np.array(beta_list))
+        hf.create_dataset('delta_pt_random_cone', data=np.array(output['delta_pt_random_cone']))
         
         for event_type in event_types:
             for jetR in jetR_list:
                 for jet_pt_bin in jet_pt_bins:
                     for R_max in max_distance_list:
                         for observable in observables:
-                            if event_type=='combined' or np.isclose(R_max, 0.):
+                            if accepted_setting(observables, event_type, R_max):
 
                                 output_key = f'{observable}_{event_type}_R{jetR}_pt{jet_pt_bin}_Rmax{R_max}'
                                 output_aggregated = output[output_key]
                                 hf.create_dataset(output_key, data=output_aggregated)
     
     print('done.')
+
+def accepted_setting(observables, event_type, R_max):
+    
+    if 'hard' in event_type and np.isclose(R_max, 0.):
+        return True
+
+    if 'combined' in event_type:
+        if 'X_four_vectors' in observables:
+            if not np.isclose(R_max, 0.):
+                return True
+        else:
+            return True
+
+    return False
 
 ##################################################################
 if __name__ == '__main__':
