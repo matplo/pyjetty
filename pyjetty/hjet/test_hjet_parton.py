954aa9d026e530640bd644d51122ecb652e01220
matplo <ploskon@gmail.com>
2021-07-16 23:19:03 -0700

    more pythia info + substructure

--- pyjetty/hjet/test_hjet_parton.py
+++ pyjetty/hjet/test_hjet_parton.py
@@ -70,6 +70,27 @@ def main():
 		perror("pythia initialization failed.")
 		return
 
+	jet_def_lund = fj.JetDefinition(fj.cambridge_algorithm, 1.0)
+	lund_gen = fjcontrib.LundGenerator(jet_def_lund)
+	print (lund_gen.description())
+	dy_groomer = fjcontrib.DynamicalGroomer(jet_def_lund)
+	print (dy_groomer.description())
+	# sd = fjcontrib.SoftDrop(0, 0.1, 1.0)
+	sd = fjcontrib.SoftDrop(0, 0.2, 1.0)
+	print (sd)
+
+	# jet_def_rc01 = fj.JetDefinition(fj.cambridge_algorithm, 0.1)
+	# jet_def_rc02 = fj.JetDefinition(fj.cambridge_algorithm, 0.2)
+	# print (jet_def_rc01)
+	# print (jet_def_rc02)
+	# rc = fjcontrib.Recluster(jet_def_rc, True)
+
+	jet_def_rc01 = fj.JetDefinition(fj.antikt_algorithm, 0.1)
+	jet_def_rc02 = fj.JetDefinition(fj.antikt_algorithm, 0.2)
+	print (jet_def_rc01)
+	print (jet_def_rc02)
+	#rc = fjcontrib.Recluster(jet_def_rc, True)
+
 	# tw = treewriter.RTreeWriter(name = 'lsjvsx', file_name = 'leadsj_vs_x.root')
 	tw = treewriter.RTreeWriter(name = 'lsjvsx', file_name = args.output)
 
@@ -107,7 +128,7 @@ def main():
 			hTT20 = hTT20s[0]
 
 		if args.no_tt is False:
-			if hTT12.perp() < 1 and hTT6.perp() < 1:
+			if hTT12.perp() < 1 and hTT6.perp() < 1 and hTT20.perp() < 1:
 				continue
 
 		jets = jet_selector(jet_def(parts))
@@ -118,18 +139,50 @@ def main():
 			j_type = match_dR(j, partons, jet_R0 / 2.)
 			if j_type[0] is None:
 				continue
-			tw.fill_branches(	j=j,
-								mult = len(parts),
-	                            hTT6=hTT6,
-	                            hTT12=hTT12,
-	                            hTT20=hTT20,
-	                            dphi6=j.delta_phi_to(hTT6),
-	                            dphi12=j.delta_phi_to(hTT12),
-	                            dphi20=j.delta_phi_to(hTT20),
-	                            ppid=j_type[0],
-	                            pquark=j_type[1],
-	                            pglue=j_type[2],  # this is redundancy
-	                            pycode=pythia.info.code()
+			j_sd = sd.result(j)
+			sd_info = fjcontrib.get_SD_jet_info(j_sd)
+			rc_sjets01 = fj.sorted_by_pt(jet_def_rc01(j.constituents()))
+			rc_sjets02 = fj.sorted_by_pt(jet_def_rc02(j.constituents()))
+			tw.fill_branches(	j 			= j,
+								mult 		= len(parts),
+
+								lund 		= [ls for ls in lund_gen.result(j)], 
+								dyg1 		= dy_groomer.result(j, 1), 
+								sd 			= j_sd, 
+								sd_z 		= sd_info.z, 
+								sd_mu 		= sd_info.mu, 
+								sd_Delta 	= sd_info.dR, 
+								lsjet01 	= rc_sjets01[0],
+								nsjet01    	= len(rc_sjets01),
+								sjet01     	= rc_sjets01,
+								lsjet02 	= rc_sjets02[0],
+								nsjet02    	= len(rc_sjets02),
+								sjet02     	= rc_sjets02,
+
+	                            hTT6 		= hTT6,
+	                            hTT12 		= hTT12,
+	                            hTT20 		= hTT20,
+	                            dphi6 		= j.delta_phi_to(hTT6),
+	                            dphi12 		= j.delta_phi_to(hTT12),
+	                            dphi20 		= j.delta_phi_to(hTT20),
+
+	                            ppid 		= j_type[0],
+	                            pquark 		= j_type[1],
+	                            pglue 		= j_type[2],  # this is redundancy
+
+	                            pycode 		= pythia.info.code(),
+	                            pysigmagen  = pythia.info.sigmaGen(),
+	                            pysigmaerr  = pythia.info.sigmaErr(),
+	                            pyid1       = pythia.info.id1pdf(),
+	                            pyid2       = pythia.info.id1pdf(),
+	                            pyx1 	    = pythia.info.x1pdf(),
+	                            pyx2       	= pythia.info.x2pdf(),
+	                            pypdf1      = pythia.info.pdf1(),
+	                            pyQfac 		= pythia.info.QFac(),
+	                            pyalphaS 	= pythia.info.alphaS(),
+
+	                            pypthat 	= pythia.info.pTHat(),
+	                            pymhat 		= pythia.info.mHat()
                     )
 			tw.fill_tree()
 
