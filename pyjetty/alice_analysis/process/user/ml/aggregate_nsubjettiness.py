281a4ad2df4f2422513578e8d70ed0df3484dc9b
James Mulligan <james.mulligan@berkeley.edu>
2021-08-12 17:26:36 -0700

    aggregate using virtual dataset

--- pyjetty/alice_analysis/process/user/ml/aggregate_nsubjettiness.py
+++ pyjetty/alice_analysis/process/user/ml/aggregate_nsubjettiness.py
@@ -10,6 +10,8 @@ import numpy as np
 
 def aggregate(config_file, filelist, output_dir):
 
+    n_files_max = 10000
+
     # List of arrays to aggregate
     observables = ['X_four_vectors', 'X_Nsub', 'y', 'jet_pt', 'delta_pt', 'matched_pt', 'matched_deltaR', 'jet_angularity', 'jet_mass', 'jet_theta_g', 'jet_subjet_z', 'hadron_z', 'multiplicity_0000', 'multiplicity_0150', 'multiplicity_0500', 'multiplicity_1000']
 
@@ -19,131 +21,167 @@ def aggregate(config_file, filelist, output_dir):
     jetR_list = config['jetR']
     jet_pt_bins = config['jet_pt_bins']
     max_distance_list = config['constituent_subtractor']['max_distance']
-    event_types = ['hard', 'combined', 'combined_matched']
+    event_types = ['hard', 'combined_matched']
 
+    # Create a list of keys to loop over
     # We have separate training data for:
     # - the hard-event and the combined-event
     # - different jet R
     # - different constituent subtraction R_max
-    
-    # Create a flat dict of empty objects for each combination
-    # We use keys equal to the keys in the input file
-    output = {}
-    output['delta_pt_random_cone'] = np.array([])
+    output_keys = []
     for event_type in event_types:
         for jetR in jetR_list:
             for jet_pt_bin in jet_pt_bins:
                 for R_max in max_distance_list:
                     for observable in observables:
-                        if accepted_setting(observables, event_type, R_max):
+                        if accepted_setting(observable, event_type, R_max):
                             output_key = f'{observable}_{event_type}_R{jetR}_pt{jet_pt_bin}_Rmax{R_max}'
-                            output[output_key] = np.array([])
+                            output_keys.append(output_key)
+
+    # We will create a virtual dataset for each combination
+    # See: https://docs.h5py.org/en/stable/vds.html
+    
+    # First, we need to find the total shape for each observable set
+    shapes, total_shapes = determine_shapes(output_keys, filelist, n_files_max)
+    print('Determined shapes.')
+
+    # Now, create the virtual dataset
+    # We use keys equal to the keys in the input file
+    if 'X_four_vectors' in observables:
+        output_filename_unshuffled = 'nsubjettiness_with_four_vectors_unshuffled.h5'
+    else:
+        output_filename_unshuffled = 'nsubjettiness_without_four_vectors_unshuffled.h5'
+    with h5py.File(os.path.join(output_dir, output_filename_unshuffled), 'w') as hf:
+        
+        for output_key in output_keys:
+            print(f'Creating virtual dataset for {output_key}')
+            print(f'  Total shape: {total_shapes[output_key]}')
+            layout = h5py.VirtualLayout(shape=total_shapes[output_key], dtype=np.float64)
+
+            # Loop through file list
+            layout_index = 0
+            with open(filelist) as f:
+                files = [line.rstrip() for line in f]
+                n_files = len(files)
+            for i,filename in enumerate(files):
+                if not accept_file(i, n_files, n_files_max, log=False):
+                    break
+
+                # Create virtual source
+                #print(f'  Creating virtual source for file {i} with shape {shapes[output_key][i]}')
+                source = h5py.VirtualSource(filename, output_key, shapes[output_key][i], dtype=np.float64)
+
+                # Insert the source into the layout
+                new_layout_index = layout_index + shapes[output_key][i][0]
+                if len(total_shapes[output_key]) == 1:
+                    layout[layout_index:new_layout_index] = source
+                elif len(total_shapes[output_key]) == 2:
+                    layout[layout_index:new_layout_index,:] = source
+                else:
+                    layout[layout_index:new_layout_index,:,:] = source
+                
+                layout_index = new_layout_index
+                #print(f'new layout_index: {layout_index}')
+                
+            # Add virtual dataset to output file
+            hf.create_virtual_dataset(output_key, layout)
+
+    print('Virtual dataset created.')
+    print()
+    
+    # Write N_list, beta_list
+    if 'X_four_vectors' in observables:
+        output_filename_shuffled = 'nsubjettiness_with_four_vectors_shuffled.h5'
+    else:
+        output_filename_shuffled = 'nsubjettiness_without_four_vectors_shuffled.h5'
+    with h5py.File(filename, 'r') as hf:
+        N_list = list(hf['N_list'][:])
+        beta_list = list(hf['beta_list'][:])
+    with h5py.File(os.path.join(output_dir, output_filename_shuffled), 'w') as hf_shuffled:
+        hf_shuffled.create_dataset('N_list', data=np.array(N_list))
+        hf_shuffled.create_dataset('beta_list', data=np.array(beta_list))
+    
+    #  Shuffle data set -- create randomized indices for each set of observables
+    idx = {}
     N_list = None
     beta_list = None
+    for output_key in output_keys:
+        print(f'Shuffling dataset for {output_key}')
+        
+        suffix = output_key.split('_', 1)[1]
+        if suffix not in idx:
+            idx[suffix] = np.array([])
+        
+        with h5py.File(os.path.join(output_dir, output_filename_unshuffled), 'r') as hf_unshuffled:
+            output = hf_unshuffled[output_key][:]
+            
+            if not np.any(idx[suffix]):
+                idx[suffix] = np.random.permutation(len(output))
 
-    # Loop through file list
+            if output.shape[0] == idx[suffix].shape[0]:
+                output_shuffled = output[idx[suffix]]
+                print(f'shuffled {output_key}: {output_shuffled.shape}')
+
+                # Remove nan
+                where_are_nan = np.isnan(output)
+                output[where_are_nan] = 0.
+            elif output.shape[0] > 0:
+                print(f'MISMATCH of shape for {output_key}: {output.shape} vs. {idx[suffix].shape}')
+                
+            with h5py.File(os.path.join(output_dir, output_filename_shuffled), 'w') as hf_shuffled:
+                hf_shuffled.create_dataset(output_key, data=output)
+
+    print('done.')
+
+# Determine shapes of lists in all files
+def determine_shapes(output_keys, filelist, n_files_max):
+
+    shapes = {}
+    shapes['delta_pt_random_cone'] = []
+    for output_key in output_keys:
+        shapes[output_key] = []
+
     with open(filelist) as f:
         files = [line.rstrip() for line in f]
         n_files = len(files)
     for i,filename in enumerate(files):
-        
-        #if i > 300:
-        #    break
-
-        if i%100 == 0:
-            print(f'{i}/{n_files}')
-
         with h5py.File(filename,'r') as hdf:
-        
-            if not N_list:
-                N_list = list(hdf['N_list'][:])
-                beta_list = list(hdf['beta_list'][:])
+            if not accept_file(i, n_files, n_files_max):
+                break
 
             delta_pt_random_cone_i = hdf['delta_pt_random_cone'][:]
-            if output['delta_pt_random_cone'].any():
-                output['delta_pt_random_cone'] = np.concatenate((output['delta_pt_random_cone'], delta_pt_random_cone_i),axis=0)
-            else:
-                output['delta_pt_random_cone'] = delta_pt_random_cone_i
-    
-            for event_type in event_types:
-                for jetR in jetR_list:
-                    for jet_pt_bin in jet_pt_bins:
-                        for R_max in max_distance_list:
-                            for observable in observables:
-                                if accepted_setting(observables, event_type, R_max):
-                            
-                                    # Get arrays from file
-                                    output_key = f'{observable}_{event_type}_R{jetR}_pt{jet_pt_bin}_Rmax{R_max}'
-                                    output_i = hdf[output_key][:]
-                                    if output_i.shape[0] > 0:
+            shapes['delta_pt_random_cone'].append(hdf['delta_pt_random_cone'][:].shape)
+            for output_key in output_keys:
+                shapes[output_key].append(hdf[output_key][:].shape)
 
-                                        # Concatenate to master array
-                                        output_aggregated = output[output_key]
-                                        if output_aggregated.any():
-                                            output[output_key] = np.concatenate((output_aggregated, output_i),axis=0)
-                                        else:
-                                            output[output_key] = output_i
+    total_shapes = {}
+    for key,val in shapes.items():
+        total_shape = np.sum([shape[0] for shape in val])
+        if len(val[0]) == 1:
+            total_shapes[key] = (total_shape,)
+        elif len(val[0]) == 2:
+            total_shapes[key] = (total_shape, 146)
+        else:
+            total_shapes[key] = (total_shape, 800, 4)
 
-                                        if i%100 == 0:
-                                            if observable in ['X_Nsub']:
-                                                print(f'{output_key} -- {output_aggregated.shape}')
+    return shapes,total_shapes
 
-    #  Shuffle data set
-    for event_type in event_types:
-        for jetR in jetR_list:
-            for jet_pt_bin in jet_pt_bins:
-                for R_max in max_distance_list:
-                    if accepted_setting(observables, event_type, R_max):
-                    
-                        output_key_y = f'y_{event_type}_R{jetR}_pt{jet_pt_bin}_Rmax{R_max}'
-                        idx = np.random.permutation(len(output[output_key_y]))
-                    
-                        for observable in observables:
+def accept_file(i, n_files, n_files_max, log=True):
+    if log:
+        if i%10 == 0:
+            print(f'{i}/{n_files}')
+    if i > n_files_max:
+        return False
+    return True
 
-                            output_key = f'{observable}_{event_type}_R{jetR}_pt{jet_pt_bin}_Rmax{R_max}'
-                            output_aggregated = output[output_key]
-                            if output_aggregated.shape[0] == idx.shape[0]:
-                                output[output_key] = output_aggregated[idx]
-                                print(f'shuffled {output_key}: {output[output_key].shape}')
-
-                                # Remove nan
-                                where_are_nan = np.isnan(output[output_key])
-                                output[output_key][where_are_nan] = 0.
-                            elif output_aggregated.shape[0] > 0:
-                                print(f'MISMATCH of shape for {output_key}: {output_aggregated.shape} vs. {idx.shape}')
-
-    # Write jet arrays to file
-    if 'X_four_vectors' in observables:
-        output_filename = 'nsubjettiness_with_four_vectors.h5'
-    else:
-        output_filename = 'nsubjettiness_without_four_vectors.h5' 
-        
-    with h5py.File(os.path.join(output_dir, output_filename), 'w') as hf:
-    
-        hf.create_dataset('N_list', data=np.array(N_list))
-        hf.create_dataset('beta_list', data=np.array(beta_list))
-        hf.create_dataset('delta_pt_random_cone', data=np.array(output['delta_pt_random_cone']))
-        
-        for event_type in event_types:
-            for jetR in jetR_list:
-                for jet_pt_bin in jet_pt_bins:
-                    for R_max in max_distance_list:
-                        for observable in observables:
-                            if accepted_setting(observables, event_type, R_max):
-
-                                output_key = f'{observable}_{event_type}_R{jetR}_pt{jet_pt_bin}_Rmax{R_max}'
-                                output_aggregated = output[output_key]
-                                hf.create_dataset(output_key, data=output_aggregated)
-    
-    print('done.')
-
-def accepted_setting(observables, event_type, R_max):
+def accepted_setting(observable, event_type, R_max):
     
     if 'hard' in event_type and np.isclose(R_max, 0.):
-        return True
+        if 'delta_pt' not in observable and 'matched' not in observable:
+            return True
 
     if 'combined' in event_type:
-        if 'X_four_vectors' in observables:
+        if 'X_four_vectors' in observable:
             if not np.isclose(R_max, 0.):
                 return True
         else:
