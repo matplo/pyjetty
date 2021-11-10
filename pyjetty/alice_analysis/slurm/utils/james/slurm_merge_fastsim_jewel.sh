1569b51d98176a991b687c8420de6dc2bcb1a898
James Mulligan <james.mulligan@berkeley.edu>
2021-11-09 20:57:42 -0800

    add jewel fastsim for subjet processing

---
+++ pyjetty/alice_analysis/slurm/utils/james/slurm_merge_fastsim_jewel.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#SBATCH --job-name=mergepthat
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=10
+#SBATCH --partition=quick
+#SBATCH --time=2:00:00
+#SBATCH --array=1-20
+#SBATCH --output=/rstorage/alice/AnalysisResults/james/slurm-%A_%a.out
+
+srun merge_fastsim_jewel.sh $SLURM_ARRAY_JOB_ID $SLURM_ARRAY_TASK_ID
