6a737b7d889c11506a3cc20e7f2d6f5837b70e74
matplo <ploskon@gmail.com>
2022-01-21 23:50:45 -0800

    zloss

---
+++ pyjetty/sandbox/run_lsj_zloss_glue.sh
@@ -0,0 +1,1 @@
+./leadsj_vs_zloss.py --jetptmin 100 --jetptmax 120 --nev 2000 --py-hardQCDgluons --out lsj_zloss_glue.root
---
+++ pyjetty/sandbox/run_lsj_zloss_quark.sh
@@ -0,0 +1,1 @@
+./leadsj_vs_zloss.py --jetptmin 100 --jetptmax 120 --nev 2000 --py-hardQCDquarks --out lsj_zloss_quark.root
