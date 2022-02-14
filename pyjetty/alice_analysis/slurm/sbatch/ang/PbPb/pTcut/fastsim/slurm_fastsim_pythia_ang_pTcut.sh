56e6339a3084f02992a2577b751f3c2a600affa8
Ezra Lesser <ezralesser@gmail.com>
2022-02-14 09:31:53 -0800

    Update scripts // add pT cut scripts for PbPb

---
+++ pyjetty/alice_analysis/slurm/sbatch/ang/PbPb/pTcut/fastsim/slurm_fastsim_pythia_ang_pTcut.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+#SBATCH --job-name="ang_fastsim"
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=3
+#SBATCH --partition=std
+#SBATCH --time=24:00:00
+#SBATCH --array=1-800
+#SBATCH --output=/rstorage/alice/AnalysisResults/ang/slurm-%A_%a.out
+
+FILE_PATHS='/rstorage/generators/pythia_alice/tree_fastsim/258314/files.txt'
+NFILES=$(wc -l < $FILE_PATHS)
+echo "N files to process: ${NFILES}"
+
+# Currently we have 8 nodes * 20 cores active
+FILES_PER_JOB=$(( $NFILES / 800 + 1 ))
+echo "Files per job: $FILES_PER_JOB"
+
+STOP=$(( SLURM_ARRAY_TASK_ID*FILES_PER_JOB ))
+START=$(( $STOP - $(( $FILES_PER_JOB - 1 )) ))
+
+if (( $STOP > $NFILES ))
+then
+  STOP=$NFILES
+fi
+
+echo "START=$START"
+echo "STOP=$STOP"
+
+for (( JOB_N = $START; JOB_N <= $STOP; JOB_N++ ))
+do
+  FILE=$(sed -n "$JOB_N"p $FILE_PATHS)
+  srun process_fastsim_pythia_ang_pTcut.sh $FILE $SLURM_ARRAY_JOB_ID $SLURM_ARRAY_TASK_ID
+done
--- pyjetty/alice_analysis/slurm/utils/ang/merge_data.sh
+++ pyjetty/alice_analysis/slurm/utils/ang/merge_data.sh
@@ -2,7 +2,7 @@
 #
 # Script to merge output ROOT files
 
-JOB_ID=890497
+JOB_ID=973422
 
 FILE_DIR="/rstorage/alice/AnalysisResults/ang/$JOB_ID"
 FILES=$( find "$FILE_DIR" -name "*.root" )
