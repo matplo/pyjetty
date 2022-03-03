c83387e101463cb0a088864d8355a45b6a8e5f3c
Ezra Lesser <ezralesser@gmail.com>
2022-03-03 13:06:12 -0800

    Script updates

--- pyjetty/alice_analysis/slurm/sbatch/ang/PbPb/fastsim/slurm_fastsim_pythia_ang.sh
+++ pyjetty/alice_analysis/slurm/sbatch/ang/PbPb/fastsim/slurm_fastsim_pythia_ang.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=3
 #SBATCH --partition=std
 #SBATCH --time=24:00:00
-#SBATCH --array=1-1000
+#SBATCH --array=1-600
 #SBATCH --output=/rstorage/alice/AnalysisResults/ang/slurm-%A_%a.out
 
 FILE_PATHS='/rstorage/generators/pythia_alice/tree_fastsim/258314/files.txt'
@@ -12,7 +12,7 @@ NFILES=$(wc -l < $FILE_PATHS)
 echo "N files to process: ${NFILES}"
 
 # Currently we have 8 nodes * 20 cores active
-FILES_PER_JOB=$(( $NFILES / 1000 + 1 ))
+FILES_PER_JOB=$(( $NFILES / 600 + 1 ))
 echo "Files per job: $FILES_PER_JOB"
 
 STOP=$(( SLURM_ARRAY_TASK_ID*FILES_PER_JOB ))
