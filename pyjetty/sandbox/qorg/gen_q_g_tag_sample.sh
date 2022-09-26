9eb64d46579660054670959905dd17f49db291e8
matplo <ploskon@gmail.com>
2022-09-26 11:33:00 -0700

    no ue added

--- pyjetty/sandbox/qorg/gen_q_g_tag_sample.sh
+++ pyjetty/sandbox/qorg/gen_q_g_tag_sample.sh
@@ -18,9 +18,9 @@ source ${HEPPY_DIR}/scripts/util.sh
 for seed in 123456
 do
 	# quarks and gluons
-	${THISD}/pythia_parton_tag.py --ml 						--py-pthatmin 500 --jet-ptmin 500 --jet-R 0.8 --jet-ptmax 550 $@
+	${THISD}/pythia_parton_tag.py --ml 						          --py-noue --py-pthatmin 500 --jet-ptmin 500 --jet-R 0.8 --jet-ptmax 550 $@
 	# only ff->gg
-	${THISD}/pythia_parton_tag.py --ml --py-hardQCDgluons 	--py-pthatmin 500 --jet-ptmin 500 --jet-R 0.8 --jet-ptmax 550 $@
+	${THISD}/pythia_parton_tag.py --ml --py-hardQCDgluons 	--py-noue --py-pthatmin 500 --jet-ptmin 500 --jet-R 0.8 --jet-ptmax 550 $@
 	# only ff->qq or ff->qqbar
-	${THISD}/pythia_parton_tag.py --ml --py-hardQCDquarks 	--py-pthatmin 500 --jet-ptmin 500 --jet-R 0.8 --jet-ptmax 550 $@
+	${THISD}/pythia_parton_tag.py --ml --py-hardQCDquarks 	--py-noue --py-pthatmin 500 --jet-ptmin 500 --jet-R 0.8 --jet-ptmax 550 $@
 done
