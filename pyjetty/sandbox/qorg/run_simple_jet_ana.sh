fc65e62e7e77422f8f8dc0d29907c0c3aa1a2968
matplo <ploskon@gmail.com>
2022-10-18 01:39:10 -0700

    plotting

--- pyjetty/sandbox/qorg/run_simple_jet_ana.sh
+++ pyjetty/sandbox/qorg/run_simple_jet_ana.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 find ${PWD}/v2_z_jets -name "*.root" | tee z_jet_files.txt
-./simple_jet_ana.py -i ./z_jet_files.txt
-./simple_jet_ana.py -i ./z_jet_files.txt --ptree tree_Particle_gen_mDT0.04 --output z_jet_files_jets_mDT0.04.root
+./simple_jet_ana.py -i ./z_jet_files.txt &
+./simple_jet_ana.py -i ./z_jet_files.txt --ptree tree_Particle_gen_mDT0.04 --output z_jet_files_jets_mDT0.04.root &
 
 find ${PWD}/v2_q_jets -name "*.root" | tee quark_jet_files.txt
-./simple_jet_ana.py -i ./quark_jet_files.txt
+./simple_jet_ana.py -i ./quark_jet_files.txt &
 
 find ${PWD}/v2_g_jets -name "*.root" | tee glue_jet_files.txt
-./simple_jet_ana.py -i ./glue_jet_files.txt
+./simple_jet_ana.py -i ./glue_jet_files.txt &
+
 
--- pyjetty/sandbox/qorg/run_v2_sample.sh
+++ pyjetty/sandbox/qorg/run_v2_sample.sh
@@ -4,17 +4,21 @@ nev=1000000
 
 this_dir=${PWD}
 
+ue_conf="--py-noue"
+ue_conf="--py-noISR"
+ue_conf="--py-noMPI"
+
 mkdir v2_z_jets
 cd v2_z_jets
-${this_dir}/pythia_gen_jets_ml_v2.py --py-noue --py-seed 123456 --Zjet --ZjetR 0.8 --mDT --mDTzcut 0.04 --py-cmnd ${this_dir}/pythia_gen_qorg_Zjet_master.cmnd --jet-ptmin 500 --jet-ptmax 550 --nev ${nev} &
+${this_dir}/pythia_gen_jets_ml_v2.py ${ue_conf} --py-seed 123456 --Zjet --ZjetR 0.8 --mDT --mDTzcut 0.04 --py-cmnd ${this_dir}/pythia_gen_qorg_Zjet_master.cmnd --jet-ptmin 500 --jet-ptmax 550 --nev ${nev} &
 cd ${this_dir}
 
 mkdir v2_g_jets
 cd v2_g_jets
-${this_dir}/pythia_gen_jets_ml_v2.py --py-noue --py-seed 123456 --py-hardQCDgluons --py-pthatmin 500 --jet-ptmin 500 --jet-ptmax 550 --nev ${nev} &
+${this_dir}/pythia_gen_jets_ml_v2.py ${ue_conf} --py-seed 123456 --py-hardQCDgluons --py-pthatmin 500 --jet-ptmin 500 --jet-ptmax 550 --nev ${nev} &
 cd ${this_dir}
 
 mkdir v2_q_jets
 cd v2_q_jets
-${this_dir}/pythia_gen_jets_ml_v2.py --py-noue --py-seed 123456 --py-hardQCDquarks --py-pthatmin 500 --jet-ptmin 500 --jet-ptmax 550 --nev ${nev} &
+${this_dir}/pythia_gen_jets_ml_v2.py ${ue_conf} --py-seed 123456 --py-hardQCDquarks --py-pthatmin 500 --jet-ptmin 500 --jet-ptmax 550 --nev ${nev} &
 cd ${this_dir}
