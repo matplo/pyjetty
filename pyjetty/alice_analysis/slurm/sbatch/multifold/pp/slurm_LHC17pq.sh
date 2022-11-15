b3f166441e6ae26c0e0d8a8b93f4d6c3beed7355
James Mulligan <james.mulligan@berkeley.edu>
2022-11-15 14:01:13 -0800

    update slurm scripts to 6 nodes

--- pyjetty/alice_analysis/slurm/sbatch/multifold/pp/slurm_LHC17pq.sh
+++ pyjetty/alice_analysis/slurm/sbatch/multifold/pp/slurm_LHC17pq.sh
@@ -4,15 +4,15 @@
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1
 #SBATCH --partition=std
 #SBATCH --time=24:00:00
-#SBATCH --array=1-160
+#SBATCH --array=1-120
 #SBATCH --output=/rstorage/alice/AnalysisResults/james/slurm-%A_%a.out
 
 FILE_PATHS='/rstorage/alice/data/LHC17pq/448/files.txt'
 NFILES=$(wc -l < $FILE_PATHS)
 echo "N files to process: ${NFILES}"
 
-# Currently we have 7 nodes * 20 cores active
-FILES_PER_JOB=$(( $NFILES / 160 + 1 ))
+# Currently we have 6 nodes * 20 cores active
+FILES_PER_JOB=$(( $NFILES / 120 + 1 ))
 echo "Files per job: $FILES_PER_JOB"
 
 STOP=$(( SLURM_ARRAY_TASK_ID*FILES_PER_JOB ))
