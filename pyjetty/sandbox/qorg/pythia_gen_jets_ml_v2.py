4c55c5376358546621020acadfe0dd35442a4515
matplo <ploskon@gmail.com>
2023-04-06 23:17:58 -0700

    fix for ml q vs glue vs Z

--- pyjetty/sandbox/qorg/pythia_gen_jets_ml_v2.py
+++ pyjetty/sandbox/qorg/pythia_gen_jets_ml_v2.py
@@ -20,125 +20,36 @@ import pythiafjext
 
 from heppy.pythiautils import configuration as pyconf
 
-import importlib
-root_avail = importlib.util.find_spec("ROOT")
-if root_avail is not None:
-	import ROOT
+from read_hepmc2_jets_ml_v2 import JetFileOutput, UniqueRunNumber
+import ROOT
 
+def run(args):
+        
+	if args.py_seed <= 0:
+		print('[e] has to speficy --py-seed ... bailing out')
+		return False
 
-class JetFileOutput(object):
-	def __init__(self, **kwargs):
-		self.args = {}
-		if 'args' in kwargs:
-			self.args = kwargs['args']
-		if type(self.args) is argparse.Namespace:	
-			self.args = vars(self.args)
-		print(self.args)
-		self.sname = []
-		for opt in self.args:
-			if self.args[opt]:
-				self.sname.append(opt)
-				if type(self.args[opt]) in [float, int]:
-					self.sname.append(str(self.args[opt]))
-		self.sname.append('.cmnd')
-		self.cmnd_file_name = '_'.join(self.sname)
-		self.nfile = 0
-  
-		self.jet_def_ca = fj.JetDefinition(fj.cambridge_algorithm, fj.JetDefinition.max_allowable_R)
-		self.mDT = None
-		try: 
-			if self.args['mDT']:
-				self.mDT = fjcontrib.ModifiedMassDropTagger(self.args['mDTzcut'])
-		except:
-			pass
-  
-	def write_file_partons(self, jets, pythia):
-		root_file_name = '_'.join(self.sname).replace('.cmnd', '_{}.root'.format(self.nfile))
-		root_file = ROOT.TFile(root_file_name, 'recreate')
-		root_ntuple_parts = ROOT.TNtuple('tree_Particle_gen', 'particles from PYTHIA8 - jets', 'run_number:ev_id:ParticlePt:ParticleEta:ParticlePhi:ParticlePID:ParticleMass')
-		root_ntuple_ev = ROOT.TNtuple('tree_Event_gen', 'event info from PYTHIA8 - jets', 'run_number:ev_id:xsec:code:partonID')
-		root_ntuple_jet = ROOT.TNtuple('tree_jet_gen', 'jet kinematics','run_number:ev_id:jetPt:jetEta:jetPhi:jetMass:ParticlePt:ParticleEta:ParticlePhi:ParticlePID:ParticleMass:dR')
-		run_number = self.args['py_seed'] + self.nfile
-		if self.args['py_seed'] < 0:
-			run_number = self.nfile
-		for i, _j in enumerate(jets):
-			ev_number_stream = i
-			j = _j[0]
-			j_flavor = _j[1]
-			j_flavor_psj = _j[2]
-			jetDR = j_flavor_psj.delta_R(j)
-			root_ntuple_ev.Fill(run_number, ev_number_stream, pythia.info.sigmaGen(), pythia.info.code(), j_flavor)
-			root_ntuple_jet.Fill(run_number, ev_number_stream, j.pt(), j.eta(), j.phi(), j.m(), j_flavor_psj.pt(), j_flavor_psj.eta(), j_flavor_psj.phi(), pythiafjext.getPythia8Particle(j_flavor_psj).id(), j_flavor_psj.m(), jetDR)
-			for p in j.constituents():
-				if pythiafjext.getPythia8Particle(p).isParton() is False:
-					root_ntuple_parts.Fill(run_number, ev_number_stream, p.pt(), p.eta(), p.phi(), pythiafjext.getPythia8Particle(p).id(), p.m())
-		self.nfile += 1
-		root_file.Write()
-		root_file.Close()
-
-	def write_file_Zjets(self, jets, pythia):
-		root_file_name = '_'.join(self.sname).replace('.cmnd', '_{}.root'.format(self.nfile))
-		root_file = ROOT.TFile(root_file_name, 'recreate')
-		root_ntuple_parts = ROOT.TNtuple('tree_Particle_gen', 'particles from PYTHIA8 - jets', 'run_number:ev_id:ParticlePt:ParticleEta:ParticlePhi:ParticlePID:ParticleMass')
-		root_ntuple_parts_mDT = ROOT.TNtuple('tree_Particle_gen_mDT{}'.format(self.args['mDTzcut']), 'particles from PYTHIA8 - jets', 'run_number:ev_id:ParticlePt:ParticleEta:ParticlePhi:ParticlePID:ParticleMass')
-		root_ntuple_ev = ROOT.TNtuple('tree_Event_gen', 'event info from PYTHIA8 - jets', 'run_number:ev_id:xsec:code')
-		root_ntuple_zjet = ROOT.TNtuple('tree_Zjet_gen', 'Zjet kinematics','run_number:ev_id:ZjetPt:ZjetEta:ZjetPhi:ZjetMass:ParticlePt:ParticleEta:ParticlePhi:ParticlePID:ParticleMass:dR')
-		run_number = self.args['py_seed'] + self.nfile
-		if self.args['py_seed'] < 0:
-			run_number = self.nfile
-		for i, _j in enumerate(jets):
-			ev_number_stream = i
-			Zjet = _j[0]
-			psjZ = _j[1]
-			ZjetDR = psjZ.delta_R(Zjet)
-			root_ntuple_ev.Fill(run_number, ev_number_stream, pythia.info.sigmaGen(), pythia.info.code())
-			root_ntuple_zjet.Fill(run_number, ev_number_stream, Zjet.pt(), Zjet.eta(), Zjet.phi(), Zjet.m(), psjZ.pt(), psjZ.eta(), psjZ.phi(), pythiafjext.getPythia8Particle(psjZ).id(), psjZ.m(), ZjetDR)
-			for p in Zjet.constituents():
-				root_ntuple_parts.Fill(run_number, ev_number_stream, p.pt(), p.eta(), p.phi(), pythiafjext.getPythia8Particle(p).id(), p.m())
-
-			# now mDT jets
-			ca_jets = fj.sorted_by_pt(self.jet_def_ca(Zjet.constituents()))
-			if (len(ca_jets) > 1):
-				print('[w] CA reclustering returns more than 1 subjet (?)')
-			else:
-				# Use modified mass-drop tagger to clean up jet.
-				taggedJet = self.mDT.result(ca_jets[0])
-				if taggedJet.has_constituents():
-					for p in taggedJet.constituents():
-						root_ntuple_parts_mDT.Fill(run_number, ev_number_stream, p.pt(), p.eta(), p.phi(), pythiafjext.getPythia8Particle(p).id(), p.m())
-		self.nfile += 1
-		root_file.Write()
-		root_file.Close()
-
-
-def main():
-	parser = argparse.ArgumentParser(
-		description='pythia8 in python', prog=os.path.basename(__file__))
-	parser.add_argument('--hepmc', help='write the hepmc files',
-	                    default=False, action='store_true')
-	parser.add_argument('--jet-R', help='specify jet R for flavor tagging', default=0.8, type=float)
-	parser.add_argument('-c', '--nperfile', help='nevents per output file', default=1000, type=int)
-	parser.add_argument('--jet-ptmin', help='minimum pT cut on jets', default=500., type=float)
-	parser.add_argument('--jet-ptmax', help='maximum pT cut on jets', default=550., type=float)
-	parser.add_argument('--Zjet', help='force Zjet generation - will read pythia_gen_qorg_Zjet_master.cmnd from current directory', default=False, action='store_true')
-	parser.add_argument('--mDT', help='mDT clean up with z_cut=0.04 (default) on the Zjets', default=False, action='store_true')
-	parser.add_argument('--mDTzcut', help='mDT clean up with z_cut=0.04 (default) on the Zjets', default=0.04, type=float)
-	# parser.add_argument('--ZjetR', help='specify the radius of the anti-kT jet for Z-match - default is R=1.0', default=1.0, type=float)
-
-	pyconf.add_standard_pythia_args(parser)
-	args = parser.parse_args()
+	nev_nperfile = args.nperfile
 
 	part_selector = fj.SelectorAbsRapMax(4.)
 	part_selector_p = fj.SelectorAbsRapMax(4.)
-	jet_selector = fj.SelectorPtMin(args.jet_ptmin) * fj.SelectorPtMax(args.jet_ptmax) * fj.SelectorAbsRapMax(1.7)
+	jet_selector = fj.SelectorPtMin(args.jet_ptmin) * fj.SelectorPtMax(args.jet_ptmax) * fj.SelectorAbsRapMax(2.5)
+
+	z_selector = fj.SelectorPtMin(args.Z_ptmin) * fj.SelectorPtMax(args.Z_ptmax) * fj.SelectorAbsRapMax(2.5)
+
+	jet_def_akt = fj.JetDefinition ( fj.antikt_algorithm, args.jet_R)
+
+	fout = JetFileOutput(args=args)
+
+	run_number = 1 # this number should change
+	urn = UniqueRunNumber()
+	run_number = urn.unique()
  
 	mycfg = []
 	pythia = pyconf.create_and_init_pythia_from_args(args, mycfg)
 	if args.nev < 10:
 		args.nev = 10
 
-	# jet_def_akt = fj.JetDefinition ( fj.antikt_algorithm, args.ZjetR)
-	jet_def_akt = fj.JetDefinition ( fj.antikt_algorithm, args.jet_R)
 	if args.Zjet is False:
 		del args.mDT
 		del args.mDTzcut
@@ -151,9 +62,7 @@ def main():
 	if args.py_bias is False:
 		del args.py_biaspow
 		del args.py_biasref
-  
-	fout = JetFileOutput(args=args)
- 
+   
 	run_number = args.py_seed
 	event_number = 0
 	pbar = tqdm.tqdm(range(args.nev))
@@ -165,7 +74,8 @@ def main():
 			continue
 		event_number = event_number + 1
    
-		parts_pythia_h = pythiafjext.vectorize_select(pythia, [pythiafjext.kFinal, pythiafjext.kVisible], 0, True)
+		# parts_pythia_h = pythiafjext.vectorize_select(pythia, [pythiafjext.kFinal, pythiafjext.kVisible], 0, True)
+		parts_pythia_h = pythiafjext.vectorize_select(pythia, [pythiafjext.kFinal], 0, True)
 		parts_pythia_h = part_selector(parts_pythia_h)
 		if len(parts_pythia_h) < 1 :
 				continue
@@ -269,9 +179,44 @@ def main():
 
 		break
 
+	if len(jets_accepted) > 0:
+		if args.Zjet is False:
+			fout.write_file_partons(jets_accepted, pythia=pythia)
+			jets_accepted.clear()
+		else:
+			fout.write_file_Zjets(jets_accepted, pythia=pythia)
+			jets_accepted.clear()
+
 	pbar.close()
 	pythia.stat()
 	pythia.settings.writeFile(fout.cmnd_file_name)
+
+def main():
+	parser = argparse.ArgumentParser(description='pythia8 in python', prog=os.path.basename(__file__))
+	parser.add_argument('--hepmc', help='write the hepmc files', default=False, action='store_true')
+	parser.add_argument('--jet-R', help='specify jet R for flavor tagging', default=0.8, type=float)
+	parser.add_argument('-c', '--nperfile', help='nevents per output file', default=1000, type=int)
+	parser.add_argument('--jet-ptmin', help='minimum pT cut on jets', default=500., type=float)
+	parser.add_argument('--jet-ptmax', help='maximum pT cut on jets', default=550., type=float)
+	parser.add_argument('--Zjet', help='force Zjet generation - will read pythia_gen_qorg_Zjet_master.cmnd from current directory', default=False, action='store_true')
+	parser.add_argument('--mDT', help='mDT clean up with z_cut=0.04 (default) on the Zjets', default=True, action='store_true')
+	parser.add_argument('--mDTzcut', help='mDT clean up with z_cut=0.04 (default) on the Zjets', default=0.04, type=float)
+
+	parser.add_argument('--Z-ptmin', help='minimum pT cut on Z', default=50., type=float)
+	parser.add_argument('--Z-ptmax', help='maximum pT cut on Z', default=550., type=float)
+
+	parser.add_argument('-i', '--input', help='hepmc file input to analyze', default=None, type=str)
+	# parser.add_argument('--nev', help='number of events to process', default=1000, type=int)
+	parser.add_argument('-l', '--list', help='treat input file as a file containing list of files to process', action='store_true', default=False)
+	parser.add_argument('-g', '--debug', help='print some extras', default=False, action='store_true')
+	parser.add_argument('-o', '--output', help='overwrite default naming of the output', default='', type=str)
+	parser.add_argument('-t', '--threads', help='mutlithread', default=1, type=int)
+	parser.add_argument('--slurm', help='get run number from slurm batch job id', action='store_true', default=False)
+
+	pyconf.add_standard_pythia_args(parser)
+	args = parser.parse_args()
+
+	run(args)
 
 if __name__ == "__main__":
 	main()
