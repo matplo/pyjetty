840a6f8918a6db791a2d57dc39ed7135c56d7c79
matplo <ploskon@gmail.com>
2023-07-07 16:28:01 -0700

    3xptbins

--- pyjetty/sandbox/qorg/run_eec_flavor.sh
+++ pyjetty/sandbox/qorg/run_eec_flavor.sh
@@ -5,6 +5,7 @@
 # 	./eec_flavor_dep_example.py --nev 1000 --py-pthatmin 20 --jet-ptmin 20 --jet-ptmax 40 --py-hardQCD${flavor} --output eec_${flavor}.root &
 # done
 # 
-parallel ./eec_flavor_dep_example.py --nev 10000 --py-pthatmin 20 --jet-ptmin 20 --jet-ptmax 40 --py-hardQCD{1} --output eec_{1}.root ::: gluons uds charm beauty
+# parallel ./eec_flavor_dep_example.py --nev 10000 --py-pthatmin 20 --jet-ptmin 20 --jet-ptmax 40 --py-hardQCD{1} --output eec_{1}.root ::: gluons uds charm beauty
 
+parallel --progress --jobs 8 ./eec_flavor_dep_example.py --nev 100 --py-pthatmin {2} --jet-ptmin {2} --jet-ptmax '$(expr {2} + 50)' --py-hardQCD{1} --output eec_{1}_{2}.root "2>&1 >" eec_{1}_{2}.log ::: gluons uds charm beauty ::: 20 100 500
 
