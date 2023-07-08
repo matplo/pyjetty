b8abb8bbb9c49b8bc1aef6c7446c885a73f5c257
matplo <ploskon@gmail.com>
2023-07-07 21:41:37 -0700

    nev

--- pyjetty/sandbox/qorg/run_eec_flavor.sh
+++ pyjetty/sandbox/qorg/run_eec_flavor.sh
@@ -7,5 +7,5 @@
 # 
 # parallel ./eec_flavor_dep_example.py --nev 10000 --py-pthatmin 20 --jet-ptmin 20 --jet-ptmax 40 --py-hardQCD{1} --output eec_{1}.root ::: gluons uds charm beauty
 
-parallel --progress --jobs 8 ./eec_flavor_dep_example.py --nev 100 --py-pthatmin {2} --jet-ptmin {2} --jet-ptmax '$(expr {2} + 50)' --py-hardQCD{1} --output eec_{1}_{2}.root "2>&1 >" eec_{1}_{2}.log ::: gluons uds charm beauty ::: 20 100 500
+parallel --progress --jobs 8 ./eec_flavor_dep_example.py --nev 10000 --py-pthatmin {2} --jet-ptmin {2} --jet-ptmax '$(expr {2} + 50)' --py-hardQCD{1} --output eec_{1}_{2}.root "2>&1 >" eec_{1}_{2}.log ::: gluons uds charm beauty ::: 20 100 500
 
