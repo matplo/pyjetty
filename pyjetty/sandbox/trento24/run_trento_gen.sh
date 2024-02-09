f61be3f904824beb58cbb66f05c6ce49cad5e3e8
matplo <ploskon@gmail.com>
2024-02-09 10:38:37 +0000

    50k

--- pyjetty/sandbox/trento24/run_trento_gen.sh
+++ pyjetty/sandbox/trento24/run_trento_gen.sh
@@ -6,7 +6,7 @@ nev=50000
 
 ./eec_lund.py --py-pthatmin 100 --jet-ptmin 100 --jet-ptmax 120 --nev ${nev} --py-hardQCDgluons --output eec_lund_gluons_100GeV_${nev}.parquet
 ./eec_lund.py --py-pthatmin 100 --jet-ptmin 100 --jet-ptmax 120 --nev ${nev} --py-hardQCDquarks --output eec_lund_quarks_100GeV_${nev}.parquet
-./eec_lund.py --py-pthatmin 100 --jet-ptmin 100 --jet-ptmax 120 --nev ${nev} --py-hardQCDusd    --output eec_lund_uds_100GeV_${nev}.parquet
+./eec_lund.py --py-pthatmin 100 --jet-ptmin 100 --jet-ptmax 120 --nev ${nev} --py-hardQCDuds    --output eec_lund_uds_100GeV_${nev}.parquet
 
 ./eec_lund.py --py-pthatmin 100 --jet-ptmin 100 --jet-ptmax 120 --nev ${nev} --py-hardQCDcharm  --output eec_lund_charm_100GeV_${nev}.parquet
 ./eec_lund.py --py-pthatmin 100 --jet-ptmin 100 --jet-ptmax 120 --nev ${nev} --py-hardQCDbeauty --output eec_lund_beauty_100GeV_${nev}.parquet
